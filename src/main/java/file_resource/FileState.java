db2e5fa945abd1cbd12a52f8be3748bfb05d9c5c
JhonnyBravo <sanfranceshika5@gmail.com>
2018-06-22 20:28:13 +0900

    modified: src/main/java/file_resource/FileState.java
    renamed methods:
      noChanged -> initStatus,
      changed -> runCommand,
      error -> errorTerminate

--- src/main/java/file_resource/FileState.java
+++ src/main/java/file_resource/FileState.java
@@ -2,17 +2,17 @@ package file_resource;
 
 public interface FileState {
     /**
-    * 変更も異常もない場合に実行するコマンドを実装する。
+    * 終了コードとメッセージの初期化コマンドを実装する。
     */
-    public void noChanged();
+    public void initStatus();
 
     /**
-    * リソースの変更が発生する場合に実行するコマンドを実装する。
+    * 実行するコマンドを実装する。
     */
-    public void changed();
+    public void runCommand();
 
     /**
     * エラーが発生した場合に実行するコマンドを実装する。
     */
-    public void error();
+    public void errorTerminate();
 }
