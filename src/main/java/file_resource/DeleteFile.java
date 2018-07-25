82f88188c66a05d8fdd0e992c6b86f4609d5f7fd
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-07-25 22:05:14 +0900

    modified: src/main/java/file_resource/DeleteFile.java
    FileState の実装を廃止。
    StatusController から終了コードとメッセージの管理メソッドを継承する方式に変更。

--- src/main/java/file_resource/DeleteFile.java
+++ src/main/java/file_resource/DeleteFile.java
@@ -5,53 +5,24 @@ import java.io.File;
 /**
  * ファイルを削除する。
  */
-public class DeleteFile implements FileState {
+public class DeleteFile extends StatusController {
     private String path;
-    private int code;
-    private String message;
 
     /**
-    * @param path 削除対象とするファイルのパスを指定する。
-    */
+     * @param path 削除対象とするファイルのパスを指定する。
+     */
     public DeleteFile(String path) {
         this.path = path;
     }
 
     /**
-    * 終了コードとメッセージを初期化する。
-    */
-    @Override
-    public void initStatus() {
-        this.code = 0;
-        this.message = null;
-    }
-
-    /**
-    * ファイルを削除する。
-    */
-    @Override
+     * ファイルを削除する。
+     */
     public void runCommand() {
         this.initStatus();
 
         new File(this.path).delete();
-        this.code = 2;
-        this.message = null;
-    }
-
-    /**
-    * エラーメッセージを出力する。
-    */
-    @Override
-    public void errorTerminate() {
-        this.code = 1;
-        System.err.println(this.message);
-    }
-
-    /**
-    * @return code 終了ステータスを返す。
-    */
-    public int getCode() {
-        return code;
+        this.setCode(2);
     }
 
 }
