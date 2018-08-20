d49520129f0e1ab64a91f051fb9e8b92a2ab2450
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-08-20 21:04:41 +0900

    modified: src/main/java/file_resource/FileController.java
    new methods: setOwner, setGroup, setMode

--- src/main/java/file_resource/FileController.java
+++ src/main/java/file_resource/FileController.java
@@ -2,6 +2,9 @@ package file_resource;
 
 import java.io.File;
 
+import group_resource.SetGroupPrincipal;
+import mode_resource.SetPermission;
+import owner_resource.SetUserPrincipal;
 import status_resource.StatusBean;
 
 /**
@@ -53,6 +56,67 @@ public class FileController extends StatusBean {
         }
 
         this.setCode(df.getCode());
+    }
+
+    /**
+     * ファイル所有者を変更する。
+     * 
+     * @param owner 新しい所有者として設定するユーザの名前を指定する。
+     */
+    public void setOwner(String owner) {
+        SetUserPrincipal sup = new SetUserPrincipal(this.path, owner);
+
+        if (new File(this.path).isFile()) {
+            sup.runCommand();
+        } else {
+            sup.initStatus();
+        }
+
+        this.setCode(sup.getCode());
+    }
+
+    /**
+     * ファイルのグループ所有者を変更する。
+     * 
+     * @param group 新しいグループ所有者として設定するグループの名前を指定する。
+     */
+    public void setGroup(String group) {
+        SetGroupPrincipal sgp = new SetGroupPrincipal(this.path, group);
+
+        if (new File(this.path).isFile()) {
+            sgp.runCommand();
+        } else {
+            sgp.initStatus();
+        }
+
+        this.setCode(sgp.getCode());
+    }
+
+    /**
+     * ファイルパーミッションを変更する。
+     * 
+     * @param mode 新しく設定するパーミッションを 3 桁の数列で指定する。
+     *             <ul>
+     *             <li>0: ---</li>
+     *             <li>1: --x</li>
+     *             <li>2: -w-</li>
+     *             <li>3: -wx</li>
+     *             <li>4: r--</li>
+     *             <li>5: r-x</li>
+     *             <li>6: rw-</li>
+     *             <li>7: rwx</li>
+     *             </ul>
+     */
+    public void setMode(String mode) {
+        SetPermission sp = new SetPermission(this.path, mode);
+
+        if (new File(this.path).isFile()) {
+            sp.runCommand();
+        } else {
+            sp.initStatus();
+        }
+
+        this.setCode(sp.getCode());
     }
 
 }
