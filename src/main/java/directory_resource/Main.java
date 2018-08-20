8e7913b73556a1ec312e3be349241b640ed2b60f
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-08-20 22:04:33 +0900

    modified: src/main/java/directory_resource/Main.java
    new options: owner, group, mode

--- src/main/java/directory_resource/Main.java
+++ src/main/java/directory_resource/Main.java
@@ -12,19 +12,32 @@ public class Main {
      *             <ul>
      *             <li>-c, --create &lt;path&gt; ディレクトリを新規作成する。</li>
      *             <li>-d, --delete &lt;path&gt; ディレクトリを削除する。</li>
+     *             <li>-o, --owner &lt;owner&gt; ディレクトリの所有者を変更する。</li>
+     *             <li>-g, --group &lt;group&gt; グループ所有者を変更する。</li>
+     *             <li>-m, --mode &lt;mode&gt; ディレクトリのパーミッションを変更する。</li>
      *             </ul>
      */
     public static void main(String[] args) {
-        LongOpt[] longopts = new LongOpt[2];
+        LongOpt[] longopts = new LongOpt[5];
         longopts[0] = new LongOpt("create", LongOpt.REQUIRED_ARGUMENT, null, 'c');
         longopts[1] = new LongOpt("delete", LongOpt.REQUIRED_ARGUMENT, null, 'd');
+        longopts[2] = new LongOpt("owner", LongOpt.REQUIRED_ARGUMENT, null, 'o');
+        longopts[3] = new LongOpt("group", LongOpt.REQUIRED_ARGUMENT, null, 'g');
+        longopts[4] = new LongOpt("mode", LongOpt.REQUIRED_ARGUMENT, null, 'm');
 
-        Getopt options = new Getopt("Main", args, "c:d:", longopts);
+        Getopt options = new Getopt("Main", args, "c:d:o:g:m:", longopts);
 
         int c;
         int cFlag = 0;
         int dFlag = 0;
+        int oFlag = 0;
+        int gFlag = 0;
+        int mFlag = 0;
+
         String path = null;
+        String owner = null;
+        String group = null;
+        String mode = null;
 
         while ((c = options.getopt()) != -1) {
             switch (c) {
@@ -36,6 +49,18 @@ public class Main {
                 path = options.getOptarg();
                 dFlag = 1;
                 break;
+            case 'o':
+                owner = options.getOptarg();
+                oFlag = 1;
+                break;
+            case 'g':
+                group = options.getOptarg();
+                gFlag = 1;
+                break;
+            case 'm':
+                mode = options.getOptarg();
+                mFlag = 1;
+                break;
             }
         }
 
@@ -43,6 +68,34 @@ public class Main {
 
         if (cFlag == 1) {
             dc.create();
+
+            if (dc.getCode() == 1) {
+                System.exit(dc.getCode());
+            }
+
+            if (oFlag == 1) {
+                dc.setOwner(owner);
+
+                if (dc.getCode() == 1) {
+                    System.exit(dc.getCode());
+                }
+            }
+
+            if (gFlag == 1) {
+                dc.setGroup(group);
+
+                if (dc.getCode() == 1) {
+                    System.exit(dc.getCode());
+                }
+            }
+
+            if (mFlag == 1) {
+                dc.setMode(mode);
+
+                if (dc.getCode() == 1) {
+                    System.exit(dc.getCode());
+                }
+            }
         } else if (dFlag == 1) {
             dc.delete();
         }
