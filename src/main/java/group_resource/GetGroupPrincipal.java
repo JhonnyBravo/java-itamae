0c6602fc191c21e62b2e7bbab679152c70455c75
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-10-24 09:32:30 +0900

    modified: src/main/java/group_resource/GetGroupPrincipal.java
    Constructer で管理していたパスの設定処理を廃止。
    代わりに init メソッドを作成し、
    DI で注入した GroupProperties クラスで管理するように修正。

--- src/main/java/group_resource/GetGroupPrincipal.java
+++ src/main/java/group_resource/GetGroupPrincipal.java
@@ -6,51 +6,61 @@ import java.nio.file.Files;
 import java.nio.file.attribute.GroupPrincipal;
 import java.nio.file.attribute.PosixFileAttributeView;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import status_resource.StatusController;
 
 /**
- * ファイルまたはディレクトリに現在設定されているグループ所有者を表す GroupPrincipal オブジェクトを生成する。
+ * ファイルまたはディレクトリに現在設定されている GroupPrincipal オブジェクト(グループ所有者を表すオブジェクト)を取得する。
  */
+@Service
 public class GetGroupPrincipal extends StatusController {
-    private String path;
+    @Autowired
+    private GroupProperties properties;
 
     /**
      * @param path 操作対象とするファイルまたはディレクトリのパスを指定する。
      */
-    public GetGroupPrincipal(String path) {
-        this.path = path;
+    public void init(String path) {
+        properties.setPath(path);
     }
 
     /**
-     * ファイルまたはディレクトリに現在設定されているグループ所有者を表す GroupPrincipal オブジェクトを返す。
+     * ファイルまたはディレクトリに現在設定されている GroupPrincipal オブジェクト(グループ所有者を表すオブジェクト)を取得する。
      * 
      * @return GroupPrincipal
      */
     public GroupPrincipal runCommand() {
         this.initStatus();
 
-        GroupPrincipal up = null;
+        GroupPrincipal principal = null;
         String osName = System.getProperty("os.name");
 
         if (osName.substring(0, 3).equals("Win")) {
-            this.setCode(1);
             this.setMessage(osName + " ではグループ所有者の取得 / 設定はできません。");
             this.errorTerminate();
-            return up;
+            return principal;
         }
 
-        PosixFileAttributeView pfa = Files.getFileAttributeView(new File(this.path).toPath(),
-                PosixFileAttributeView.class);
+        File f = new File(properties.getPath());
+
+        if (!f.exists()) {
+            this.setMessage(properties.getPath() + " が見つかりません。");
+            this.errorTerminate();
+            return principal;
+        }
+
+        PosixFileAttributeView pfa = Files.getFileAttributeView(f.toPath(), PosixFileAttributeView.class);
 
         try {
-            up = pfa.readAttributes().group();
+            principal = pfa.readAttributes().group();
             this.setCode(2);
         } catch (IOException e) {
-            this.setCode(1);
             this.setMessage("エラーが発生しました。 " + e.toString());
             this.errorTerminate();
         }
 
-        return up;
+        return principal;
     }
 }
