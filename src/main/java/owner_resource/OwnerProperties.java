928410c508d124b33a29eadc5864d6ffe1ffe1ca
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-10-24 21:56:17 +0900

    new file: src/main/java/owner_resource/OwnerProperties.java
    Constructer で管理していたパスとユーザ名の入出力処理を
    OwnerProperties クラスへ分離。

---
+++ src/main/java/owner_resource/OwnerProperties.java
@@ -0,0 +1,40 @@
+package owner_resource;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ファイルまたはディレクトリの所有者の変更に使用するプロパティを管理する。
+ */
+@Service
+public class OwnerProperties {
+    private String path;
+    private String owner;
+
+    /**
+     * @return path 操作対象とするファイルまたはディレクトリのパスを返す。
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * @param path 操作対象とするファイルまたはディレクトリのパスを指定する。
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * @return owner ファイルまたはディレクトリの所有者を返す。
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * @param owner ファイルまたはディレクトリの所有者として設定するユーザ名を指定する。
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
