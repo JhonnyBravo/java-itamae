b126e00bbf86c4b00f1fa57942b8af5a2cf19b32
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-12-15 15:55:20 +0900

    modified: src/test/java/owner_resource/GetTestProperties.java
    継承元クラスを StatusController から Status へ変更。
    init メソッドを削除。

--- src/test/java/owner_resource/GetTestProperties.java
+++ src/test/java/owner_resource/GetTestProperties.java
@@ -6,22 +6,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import status_resource.StatusController;
+import status_resource.Status;
 
 /**
  * テスト時に使用するプロパティを外部ファイルから取得する。
  */
-public class GetTestProperties extends StatusController {
-    // GroupProperties gp = new GroupProperties();
-    private OwnerProperties op = new OwnerProperties();
+public class GetTestProperties extends Status {
     private Properties p = new Properties();
-
-    /**
-     * @param path プロパティ取得対象とするファイルのパスを指定する。
-     */
-    public void init(String path) {
-        op.setPath(path);
-    }
 
     /**
      * @param path InputStream 取得対象とするファイルのパスを指定する。
@@ -37,20 +28,22 @@ public class GetTestProperties extends StatusController {
             this.setCode(2);
             return input;
         } catch (FileNotFoundException e) {
-            this.setMessage("エラーが発生しました。 " + e.toString());
-            this.errorTerminate();
+            this.errorTerminate("エラーが発生しました。 " + e.toString());
             return input;
         }
     }
 
     /**
+     * プロパティファイルからテストに使用するユーザ名を取得する。
+     * 
+     * @param path 読込対象とするファイルのパスを指定する。
      * @return ownerName テスト時に使用するユーザー名を返す。
      */
-    public String getOwnerName() {
+    public String getOwnerName(String path) {
         this.initStatus();
 
         String ownerName = null;
-        InputStream input = getInputStream(op.getPath());
+        InputStream input = getInputStream(path);
 
         if (this.getCode() == 1) {
             return ownerName;
@@ -60,14 +53,12 @@ public class GetTestProperties extends StatusController {
             p.load(input);
             ownerName = p.getProperty("ownerName");
         } catch (IOException e) {
-            this.setMessage("エラーが発生しました。 " + e.toString());
-            this.errorTerminate();
+            this.errorTerminate("エラーが発生しました。 " + e.toString());
             return ownerName;
         }
 
         if (ownerName == null) {
-            this.setMessage("ownerName プロパティが見つかりません。");
-            this.errorTerminate();
+            this.errorTerminate("ownerName プロパティが見つかりません。");
             return ownerName;
         }
 
