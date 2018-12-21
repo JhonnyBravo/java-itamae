ce846868967178c81cd19fa9f49adcba340de6ac
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-12-21 17:09:26 +0900

    modified: src/main/java/file_resource/FileController.java
    依存クラスの仕様変更に伴うコード修正。

--- src/main/java/file_resource/FileController.java
+++ src/main/java/file_resource/FileController.java
@@ -1,135 +1,153 @@
 package file_resource;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 
-import group_resource.SetGroupPrincipal;
-import mode_resource.SetPermission;
-import owner_resource.SetUserPrincipal;
-import status_resource.StatusController;
+import group_resource.Group;
+import mode_resource.Mode;
+import owner_resource.Owner;
+import status_resource.Status;
 
 /**
- * ファイルの作成 / 削除とファイル所有者の変更 / グループ所有者の変更 / ファイルパーミッションの変更を実行する。
+ * @see file_resource.FileResource
  */
 @Service
-@Import({ SetGroupPrincipal.class, SetPermission.class, SetUserPrincipal.class })
-public class FileController extends StatusController {
-    @Autowired
-    private FileProperties fp;
+@Import({ Group.class, Mode.class, Owner.class })
+public class FileController extends Status implements FileResource {
+    private String path = null;
+    private File file = null;
 
     @Autowired
-    private CreateFile cf;
+    private Owner ownerResource;
 
     @Autowired
-    private DeleteFile df;
+    private Group groupResource;
 
     @Autowired
-    private SetUserPrincipal sup;
-
-    @Autowired
-    private SetGroupPrincipal sgp;
-
-    @Autowired
-    private SetPermission sp;
+    private Mode modeResource;
 
     /**
-     * @param path 操作対象とするファイルのパスを指定する。
+     * @see file_resource.FileResource#setOwner(String)
      */
-    public void init(String path) {
-        fp.setPath(path);
-    }
-
-    /**
-     * <ul>
-     * <li>ファイルが既に存在する場合: 何もしない。</li>
-     * <li>ファイルが存在しない場合: ファイルを新規作成する。</li>
-     * </ul>
-     */
-    public void create() {
-        cf.init(fp.getPath());
-        cf.runCommand();
-        this.setCode(cf.getCode());
-    }
-
-    /**
-     * <ul>
-     * <li>ファイルが存在する場合: ファイルを削除する。</li>
-     * <li>ファイルが存在しない場合: 何もしない。</li>
-     * </ul>
-     */
-    public void delete() {
-        df.init(fp.getPath());
-        df.runCommand();
-        this.setCode(df.getCode());
-    }
-
-    /**
-     * ファイル所有者を変更する。
-     * 
-     * @param owner 新しい所有者として設定するユーザの名前を指定する。
-     */
+    @Override
     public void setOwner(String owner) {
-        File f = new File(fp.getPath());
+        this.initStatus();
 
-        if (!f.isFile()) {
-            this.setMessage(fp.getPath() + " が見つかりません。");
-            this.errorTerminate();
+        if (this.path == null) {
+            this.errorTerminate("path を指定してください。");
             return;
         }
 
-        sup.init(fp.getPath(), owner);
-        sup.runCommand();
-        this.setCode(sup.getCode());
+        if (!this.file.isFile()) {
+            this.errorTerminate(this.path + " が見つかりません。");
+            return;
+        }
+
+        ownerResource.setOwner(this.path, owner);
+        this.setCode(ownerResource.getCode());
     }
 
     /**
-     * ファイルのグループ所有者を変更する。
-     * 
-     * @param group 新しいグループ所有者として設定するグループの名前を指定する。
+     * @see file_resource.FileResource#setGroup(String)
      */
+    @Override
     public void setGroup(String group) {
-        File f = new File(fp.getPath());
+        this.initStatus();
 
-        if (!f.isFile()) {
-            this.setMessage(fp.getPath() + " が見つかりません。");
-            this.errorTerminate();
+        if (this.path == null) {
+            this.errorTerminate("path を指定してください。");
             return;
         }
 
-        sgp.init(fp.getPath(), group);
-        sgp.runCommand();
-        this.setCode(sgp.getCode());
+        if (!this.file.isFile()) {
+            this.errorTerminate(this.path + " が見つかりません。");
+            return;
+        }
+
+        groupResource.setGroup(this.path, group);
+        this.setCode(groupResource.getCode());
     }
 
     /**
-     * ファイルパーミッションを変更する。
-     * 
-     * @param mode 新しく設定するパーミッションを 3 桁の数列で指定する。
-     *             <ul>
-     *             <li>0: ---</li>
-     *             <li>1: --x</li>
-     *             <li>2: -w-</li>
-     *             <li>3: -wx</li>
-     *             <li>4: r--</li>
-     *             <li>5: r-x</li>
-     *             <li>6: rw-</li>
-     *             <li>7: rwx</li>
-     *             </ul>
+     * @see file_resource.FileResource#setMode(String)
      */
+    @Override
     public void setMode(String mode) {
-        File f = new File(fp.getPath());
+        this.initStatus();
 
-        if (!f.isFile()) {
-            this.setMessage(fp.getPath() + " が見つかりません。");
-            this.errorTerminate();
+        if (this.path == null) {
+            this.errorTerminate("path を指定してください。");
             return;
         }
 
-        sp.init(fp.getPath(), mode);
-        sp.runCommand();
-        this.setCode(sp.getCode());
+        if (!this.file.isFile()) {
+            this.errorTerminate(this.path + " が見つかりません。");
+            return;
+        }
+
+        modeResource.setMode(this.path, mode);
+        this.setCode(modeResource.getCode());
+    }
+
+    /**
+     * @see file_resource.FileResource#setPath(String)
+     */
+    @Override
+    public void setPath(String path) {
+        this.path = path;
+        this.file = new File(this.path);
+    }
+
+    /**
+     * @see file_resource.FileResource#createFile()
+     */
+    @Override
+    public void createFile() {
+        this.initStatus();
+
+        if (this.path == null) {
+            this.errorTerminate("path を指定してください。");
+            return;
+        }
+
+        if (this.file.isFile()) {
+            this.initStatus();
+            return;
+        } else {
+            System.out.println(this.path + " を作成します。");
+
+            try {
+                this.file.createNewFile();
+                this.setCode(2);
+            } catch (IOException e) {
+                this.errorTerminate("エラーが発生しました。 " + e.toString());
+            }
+        }
+    }
+
+    /**
+     * @see file_resource.FileResource#deleteFile()
+     */
+    @Override
+    public void deleteFile() {
+        this.initStatus();
+
+        if (this.path == null) {
+            this.errorTerminate("path を指定してください。");
+            return;
+        }
+
+        if (!this.file.isFile()) {
+            this.initStatus();
+            return;
+        } else {
+            System.out.println(this.path + " を削除します。");
+            this.file.delete();
+            this.setCode(2);
+        }
     }
 }
