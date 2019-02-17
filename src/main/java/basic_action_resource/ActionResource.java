2f8548348a9404a8a8f465df0e3fd9d8f7edcaa7
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-02-17 20:45:16 +0900

    modified: src/main/java/basic_action_resource/ActionResource.java
    fix #1
    new methods: setOwner, setGroup, setMode

--- src/main/java/basic_action_resource/ActionResource.java
+++ src/main/java/basic_action_resource/ActionResource.java
@@ -9,4 +9,10 @@ public abstract class ActionResource extends Status {
     public abstract void create();
 
     public abstract void delete();
+
+    public abstract void setOwner(String owner);
+
+    public abstract void setGroup(String group);
+
+    public abstract void setMode(String mode);
 }
