8c4d07c0256b346b33dea9a82fd4041a2b996fae
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-23 05:31:49 +0900

    modified: src/main/java/attribute_resource/GroupResource.java
    ジェネリクスと Logger を導入。例外処理設定を追記。

--- src/main/java/attribute_resource/GroupResource.java
+++ src/main/java/attribute_resource/GroupResource.java
@@ -1,21 +1,24 @@
 package attribute_resource;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.attribute.GroupPrincipal;
 import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.UserPrincipalLookupService;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
 
 /**
  * ファイルまたはディレクトリのグループ所有者を管理する。
  */
-public class GroupResource extends AttributeResource {
-    private String path;
-    private String group;
-    private File file;
+public class GroupResource extends AttributeResource<GroupPrincipal> {
+    private final Logger logger;
+    private final String path;
+    private final String group;
+    private final File file;
 
     /**
      * @param path  操作対象とするファイルまたはディレクトリのパスを指定する。
@@ -24,103 +27,87 @@ public class GroupResource extends AttributeResource {
     public GroupResource(String path, String group) {
         this.path = path;
         this.group = group;
-        this.file = new File(path);
+        file = new File(path);
+
+        logger = Logger.getLogger(this.getClass().getName());
+        logger.addHandler(new ConsoleHandler());
     }
 
     /**
-     * @return Object ファイルまたはディレクトリに現在のグループ所有者として設定されている GroupPrincipal を取得する。
-     * 
-     * @see attribute_resource.AttributeResource#getAttribute()
+     * @return GroupPrincipal 新規生成された GroupPrincipal を返す。
+     * @throws IOException {@link java.io.IOException}
+     *
+     * @see attribute_resource.AttributeResource#createAttribute()
      */
     @Override
-    protected Object getAttribute() {
-        this.initStatus();
-
-        GroupPrincipal gp = null;
-
-        if (!this.file.exists()) {
-            this.errorTerminate(this.path + " が見つかりません。");
-            return gp;
-        }
-
-        PosixFileAttributeView pfav = Files.getFileAttributeView(this.file.toPath(), PosixFileAttributeView.class);
-
-        try {
-            gp = pfav.readAttributes().group();
-            this.setCode(2);
-        } catch (IOException e) {
-            this.errorTerminate("エラーが発生しました。 " + e);
-        }
+    protected GroupPrincipal createAttribute() throws IOException {
+        final UserPrincipalLookupService upls = FileSystems.getDefault().getUserPrincipalLookupService();
+        final GroupPrincipal gp = upls.lookupPrincipalByGroupName(group);
 
         return gp;
     }
 
     /**
-     * @return Object 新規生成された GroupPrincipal を返す。
-     * 
-     * @see attribute_resource.AttributeResource#createAttribute()
+     * @return GroupPrincipal ファイルまたはディレクトリに現在のグループ所有者として設定されている GroupPrincipal
+     *         を取得する。
+     * @throws IOException           {@link java.io.IOException}
+     * @throws FileNotFoundException {@link java.io.FileNotFoundException}
+     * @see attribute_resource.AttributeResource#getAttribute()
      */
     @Override
-    protected Object createAttribute() {
-        this.initStatus();
-
-        FileSystem fs = FileSystems.getDefault();
-        UserPrincipalLookupService upls = fs.getUserPrincipalLookupService();
-        GroupPrincipal gp = null;
-
-        try {
-            gp = upls.lookupPrincipalByGroupName(this.group);
-            this.setCode(2);
-        } catch (IOException e) {
-            this.errorTerminate("エラーが発生しました。 " + e);
+    protected GroupPrincipal getAttribute() throws FileNotFoundException, IOException {
+        if (!file.exists()) {
+            throw new FileNotFoundException(path + " が見つかりません。");
         }
+
+        final PosixFileAttributeView pfav = Files.getFileAttributeView(file.toPath(), PosixFileAttributeView.class);
+        final GroupPrincipal gp = pfav.readAttributes().group();
 
         return gp;
     }
 
     /**
      * ファイルまたはディレクトリのグループ所有者を変更する。
-     * 
+     *
+     * @throws FileNotFoundException {@link java.io.FileNotFoundException}
+     * @throws IOException           {@link java.io.IOException}
      * @see attribute_resource.AttributeResource#setAttribute()
+     * @return status
+     *         <ul>
+     *         <li>true: ファイルまたはディレクトリのグループ所有者を変更したことを表す。</li>
+     *         <li>false: ファイルまたはディレクトリのグループ所有者を変更していないことを表す。</li>
+     *         </ul>
      */
     @Override
-    public void setAttribute() {
-        this.initStatus();
+    public boolean setAttribute() throws FileNotFoundException, IOException {
+        boolean status = false;
+        logger.entering(this.getClass().getName(), "setAttribute");
 
-        String osName = System.getProperty("os.name");
+        final String osName = System.getProperty("os.name");
 
         if (osName.substring(0, 3).equals("Win")) {
-            this.errorTerminate(osName + " ではグループ所有者の取得 / 設定はサポートしていません。");
-            return;
+            logger.warning(osName + " ではグループ所有者の取得 / 設定はサポートしていません。");
+            return status;
         }
 
-        GroupPrincipal curGroup = (GroupPrincipal) this.getAttribute();
-
-        if (this.getCode() == 1) {
-            return;
-        }
-
-        GroupPrincipal newGroup = (GroupPrincipal) this.createAttribute();
-
-        if (this.getCode() == 1) {
-            return;
-        }
+        final GroupPrincipal curGroup = getAttribute();
+        final GroupPrincipal newGroup = createAttribute();
 
         if (curGroup.equals(newGroup)) {
-            this.initStatus();
-            return;
+            return status;
         }
 
-        System.out.println("グループ所有者を変更します。");
+        logger.info("グループ所有者を変更します。");
 
-        PosixFileAttributeView pfav = Files.getFileAttributeView(this.file.toPath(), PosixFileAttributeView.class);
+        final PosixFileAttributeView pfav = Files.getFileAttributeView(file.toPath(), PosixFileAttributeView.class);
 
         try {
             pfav.setGroup(newGroup);
-            this.setCode(2);
-        } catch (IOException e) {
-            this.errorTerminate("エラーが発生しました。 " + e);
+            status = true;
+            return status;
+        } catch (final IOException e) {
+            logger.throwing(this.getClass().getName(), "setAttribute", e);
+            throw e;
         }
     }
-
 }
