947346ac7a621248472b18a534befb61cef448d3
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-23 05:40:01 +0900

    modified: src/test/java/attribute_resource/OwnerResourceTest.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/attribute_resource/OwnerResourceTest.java
+++ src/test/java/attribute_resource/OwnerResourceTest.java
@@ -4,6 +4,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.junit.After;
@@ -17,65 +19,15 @@ import context_resource.PropertiesResource;
 
 @RunWith(Enclosed.class)
 public class OwnerResourceTest {
-    public static class ファイルの所有者を変更する場合 {
-        private String ownerName;
-        private File file = new File("test.txt");
-
-        @Before
-        public void setUp() throws Exception {
-            ContextResource cr = new PropertiesResource("src/test/resources/test.properties");
-            cr.openContext();
-            Properties p = (Properties) cr.getContext();
-            ownerName = p.getProperty("ownerName");
-            cr.closeContext();
-
-            file.createNewFile();
-        }
-
-        @After
-        public void tearDown() throws Exception {
-            file.delete();
-        }
-
-        @Test
-        public void ファイル所有者の変更ができて終了コードが2であること() {
-            AttributeResource ar = new OwnerResource("test.txt", ownerName);
-            ar.setAttribute();
-            assertThat(ar.getCode(), is(2));
-        }
-
-        @Test
-        public void 新しいファイル所有者として設定するユーザ名が現在のファイル所有者として設定されているユーザ名と同一である場合に終了コードが0であること() {
-            AttributeResource ar = new OwnerResource("test.txt", ownerName);
-            ar.setAttribute();
-            ar.setAttribute();
-            assertThat(ar.getCode(), is(0));
-        }
-
-        @Test
-        public void 存在しないファイルのファイル所有者を変更しようとした場合にエラーとなり終了コードが1であること() {
-            AttributeResource ar = new OwnerResource("NotExist.txt", ownerName);
-            ar.setAttribute();
-            assertThat(ar.getCode(), is(1));
-        }
-
-        @Test
-        public void 新しいファイル所有者として存在しないユーザの名前を指定した場合にエラーとなり終了コードが1であること() {
-            AttributeResource ar = new OwnerResource("test.txt", "NotExist");
-            ar.setAttribute();
-            assertThat(ar.getCode(), is(1));
-        }
-    }
-
     public static class ディレクトリの所有者を変更する場合 {
         private String ownerName;
-        private File file = new File("testDir");
+        private final File file = new File("testDir");
 
         @Before
         public void setUp() throws Exception {
-            ContextResource cr = new PropertiesResource("src/test/resources/test.properties");
+            final ContextResource cr = new PropertiesResource("src/test/resources/test.properties");
             cr.openContext();
-            Properties p = (Properties) cr.getContext();
+            final Properties p = (Properties) cr.getContext();
             ownerName = p.getProperty("ownerName");
             cr.closeContext();
 
@@ -88,32 +40,84 @@ public class OwnerResourceTest {
         }
 
         @Test
-        public void ディレクトリ所有者の変更ができて終了コードが2であること() {
-            AttributeResource ar = new OwnerResource("testDir", ownerName);
+        public void ディレクトリ所有者の変更ができて終了ステータスがtrueであること() throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("testDir", ownerName);
+            final boolean status = ar.setAttribute();
+            assertThat(status, is(true));
+        }
+
+        @Test(expected = IOException.class)
+        public void 新しいディレクトリ所有者として存在しないユーザの名前を指定した場合にエラーとなりIOExceptionが送出されること()
+                throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("testDir", "NotExist");
             ar.setAttribute();
-            assertThat(ar.getCode(), is(2));
         }
 
         @Test
-        public void 新しいディレクトリ所有者として設定するユーザ名が現在のディレクトリ所有者として設定されているユーザ名と同一である場合に終了コードが0であること() {
-            AttributeResource ar = new OwnerResource("testDir", ownerName);
+        public void 新規設定するディレクトリ所有者のユーザ名が現在設定されているディレクトリ所有者のユーザ名と同一である場合に終了ステータスがfalseであること()
+                throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("testDir", ownerName);
             ar.setAttribute();
+            final boolean status = ar.setAttribute();
+            assertThat(status, is(false));
+        }
+
+        @Test(expected = FileNotFoundException.class)
+        public void 存在しないディレクトリの所有者を変更しようとした場合にエラーとなりFileNotFoundExceptionが送出されること()
+                throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("NotExist", ownerName);
             ar.setAttribute();
-            assertThat(ar.getCode(), is(0));
+        }
+    }
+
+    public static class ファイルの所有者を変更する場合 {
+        private String ownerName;
+        private final File file = new File("test.txt");
+
+        @Before
+        public void setUp() throws Exception {
+            final ContextResource cr = new PropertiesResource("src/test/resources/test.properties");
+            cr.openContext();
+            final Properties p = (Properties) cr.getContext();
+            ownerName = p.getProperty("ownerName");
+            cr.closeContext();
+
+            file.createNewFile();
+        }
+
+        @After
+        public void tearDown() throws Exception {
+            file.delete();
         }
 
         @Test
-        public void 存在しないディレクトリの所有者を変更しようとした場合にエラーとなり終了コードが1であること() {
-            AttributeResource ar = new OwnerResource("NotExist", ownerName);
+        public void ファイル所有者の変更ができて終了ステータスがtrueであること() throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("test.txt", ownerName);
+            final boolean status = ar.setAttribute();
+            assertThat(status, is(true));
+        }
+
+        @Test(expected = IOException.class)
+        public void 新しいファイル所有者として存在しないユーザの名前を指定した場合にエラーとなりIOExceptionが送出されること()
+                throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("test.txt", "NotExist");
             ar.setAttribute();
-            assertThat(ar.getCode(), is(1));
         }
 
         @Test
-        public void 新しいディレクトリ所有者として存在しないユーザの名前を指定した場合にエラーとなり終了コードが1であること() {
-            AttributeResource ar = new OwnerResource("testDir", "NotExist");
+        public void 新規設定するファイル所有者のユーザ名が現在のファイル所有者のユーザ名と同一である場合に終了ステータスがfalseであること()
+                throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("test.txt", ownerName);
             ar.setAttribute();
-            assertThat(ar.getCode(), is(1));
+            final boolean status = ar.setAttribute();
+            assertThat(status, is(false));
+        }
+
+        @Test(expected = FileNotFoundException.class)
+        public void 存在しないファイルのファイル所有者を変更しようとした場合にエラーとなりFileNotFoundExceptionが送出されること()
+                throws FileNotFoundException, IOException {
+            final AttributeResource<?> ar = new OwnerResource("NotExist.txt", ownerName);
+            ar.setAttribute();
         }
     }
 }
