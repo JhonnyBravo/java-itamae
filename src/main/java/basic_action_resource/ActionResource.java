b2be4416e0b18dd37952dc0617b296dcdb9d6326
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-09-26 03:23:24 +0900

    modified: src/main/java/basic_action_resource/ActionResource.java
    抽象クラスを廃止。インターフェイスへ変更。

--- src/main/java/basic_action_resource/ActionResource.java
+++ src/main/java/basic_action_resource/ActionResource.java
@@ -1,19 +1,16 @@
 package basic_action_resource;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 /**
  * リソースの作成・削除と、所有者・グループ所有者・パーミッションの設定変更を実行する。
  */
-public abstract class ActionResource {
-    public abstract boolean create() throws IOException;
+public interface ActionResource {
+    public abstract boolean create() throws Exception;
 
     public abstract boolean delete();
 
-    public abstract boolean setOwner(String owner) throws FileNotFoundException, IOException;
+    public abstract boolean setOwner(String owner) throws Exception;
 
-    public abstract boolean setGroup(String group) throws FileNotFoundException, IOException;
+    public abstract boolean setGroup(String group) throws Exception;
 
-    public abstract boolean setMode(String mode) throws FileNotFoundException, IOException;
+    public abstract boolean setMode(String mode) throws Exception;
 }
