5239424e0e77c6538722c94f84c8dfcef0e8f074
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-10-18 02:32:07 +0900

    renamed package: src/test/java/basic_action_resource/domain/{repository -> repository/action}

--- src/test/java/basic_action_resource/domain/repository/action/DirectoryResourceTest.java
+++ src/test/java/basic_action_resource/domain/repository/action/DirectoryResourceTest.java
@@ -1,4 +1,4 @@
-package basic_action_resource.domain.repository;
+package basic_action_resource.domain.repository.action;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -13,8 +13,8 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-import basic_action_resource.domain.repository.ActionResource;
-import basic_action_resource.domain.repository.DirectoryResource;
+import basic_action_resource.domain.repository.action.ActionResource;
+import basic_action_resource.domain.repository.action.DirectoryResource;
 
 @RunWith(Enclosed.class)
 public class DirectoryResourceTest {
