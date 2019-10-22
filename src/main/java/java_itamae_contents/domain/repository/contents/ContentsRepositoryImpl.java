51995a6f2e883d5151f8c4571ec4d64e4bb782fc
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-10-22 17:28:47 +0900

    renamed: src/main/java/{content_resource/domain/repository/TxtResource.java} -> {java_itamae_contents/domain/repository/contents/ContentsRepositoryImpl.java}

---
+++ src/main/java/java_itamae_contents/domain/repository/contents/ContentsRepositoryImpl.java
@@ -0,0 +1,54 @@
+package java_itamae_contents.domain.repository.contents;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContentsRepositoryImpl implements ContentsRepository {
+
+    @Override
+    public List<String> getContents(Reader reader) throws Exception {
+        final List<String> contents = new ArrayList<String>();
+
+        try (BufferedReader buffer = new BufferedReader(reader)) {
+            String line;
+
+            while ((line = buffer.readLine()) != null) {
+                contents.add(line);
+            }
+        }
+
+        return contents;
+    }
+
+    @Override
+    public boolean setContents(Writer writer, List<String> contents) throws Exception {
+        boolean status = false;
+
+        try (BufferedWriter buffer = new BufferedWriter(writer)) {
+            for (final String line : contents) {
+                buffer.write(line);
+                buffer.newLine();
+            }
+
+            status = true;
+        }
+
+        return status;
+    }
+
+    @Override
+    public boolean deleteContents(Writer writer) throws Exception {
+        boolean status = false;
+
+        try (BufferedWriter buffer = new BufferedWriter(writer)) {
+            buffer.write("");
+            status = true;
+        }
+
+        return status;
+    }
+}
