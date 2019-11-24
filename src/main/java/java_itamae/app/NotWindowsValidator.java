8daef7889a9d701bc493cc7273f6d82988c3e917
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-25 03:10:09 +0900

    renamed: src/main/java/java_itamae/app/{OsValidator.java -> NotWindowsValidator.java}
    アノテーション付与対象をクラスとする相関チェック方式を廃止。
    アノテーション付与対象をフィールドとする単項目チェック方式に変更。

---
+++ src/main/java/java_itamae/app/NotWindowsValidator.java
@@ -0,0 +1,24 @@
+package java_itamae.app;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class NotWindowsValidator implements ConstraintValidator<NotWindows, String> {
+
+    @Override
+    public void initialize(NotWindows constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        boolean result = true;
+
+        final String osName = System.getProperty("os.name");
+
+        if (value != null && osName.substring(0, 3).equals("Win")) {
+            result = false;
+        }
+
+        return result;
+    }
+}
