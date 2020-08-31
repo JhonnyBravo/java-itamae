08a92ebe54363c6ba041efe051f6adb02cf680be
JhonnyBravo <sanfranceshika5@gmail.com>
2020-08-31 18:33:29 +0900

    new file: src/main/java/java_itamae/app/file/DeleteFile.java

---
+++ src/main/java/java_itamae/app/file/DeleteFile.java
@@ -0,0 +1,47 @@
+package java_itamae.app.file;
+
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java_itamae.domain.model.Attribute;
+import java_itamae.domain.service.file.FileService;
+import java_itamae.domain.service.file.FileServiceImpl;
+
+/**
+ * ファイルを削除する。
+ */
+public class DeleteFile implements Function<Attribute, Integer> {
+    /**
+     * ファイルを削除する。
+     *
+     * @param attr
+     *            操作対象とするファイルの情報を収めた Attribute を指定する。
+     * @return status
+     *         <ul>
+     *         <li>0: 操作を実行しなかったことを表す。</li>
+     *         <li>1: エラーが発生したことを表す。</li>
+     *         <li>2: 操作を実行したことを表す。</li>
+     *         </ul>
+     */
+    @Override
+    public Integer apply(Attribute attr) {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        final FileService service = new FileServiceImpl();
+
+        try {
+            boolean result = service.delete(attr);
+
+            if (result) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            logger.warn(e.toString());
+            return 1;
+        }
+    }
+
+}
