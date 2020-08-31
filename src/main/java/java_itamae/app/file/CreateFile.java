c2a825d2ff0d59557dc0544ce78b7712a1552c3f
JhonnyBravo <sanfranceshika5@gmail.com>
2020-08-31 18:32:51 +0900

    new file: src/main/java/java_itamae/app/file/CreateFile.java

---
+++ src/main/java/java_itamae/app/file/CreateFile.java
@@ -0,0 +1,59 @@
+package java_itamae.app.file;
+
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java_itamae.domain.model.Attribute;
+import java_itamae.domain.service.file.FileService;
+import java_itamae.domain.service.file.FileServiceImpl;
+
+/**
+ * ファイルに対して以下の操作を実行する。
+ * <ul>
+ * <li>新規作成</li>
+ * <li>所有者の変更</li>
+ * <li>グループ所有者の変更</li>
+ * <li>パーミッションの変更</li>
+ * </ul>
+ */
+public class CreateFile implements Function<Attribute, Integer> {
+    /**
+     * ファイルに対して以下の操作を実行する。
+     * <ul>
+     * <li>新規作成</li>
+     * <li>所有者の変更</li>
+     * <li>グループ所有者の変更</li>
+     * <li>パーミッションの変更</li>
+     * </ul>
+     *
+     * @param attr
+     *            操作対象とするファイルの情報を収めた Attribute を指定する。
+     * @return status
+     *         <ul>
+     *         <li>0: 操作を実行しなかったことを表す。</li>
+     *         <li>1: エラーが発生したことを表す。</li>
+     *         <li>2: 操作を実行したことを表す。</li>
+     *         </ul>
+     */
+    @Override
+    public Integer apply(Attribute attr) {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        final FileService service = new FileServiceImpl();
+
+        try {
+            boolean result = service.create(attr);
+
+            if (result) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            logger.warn(e.toString());
+            return 1;
+        }
+    }
+
+}
