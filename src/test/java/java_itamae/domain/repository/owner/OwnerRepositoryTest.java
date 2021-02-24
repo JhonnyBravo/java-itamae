828b06671aad0a4dadd40acce4e0a0aabb8d2b19
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-02-24 22:07:15 +0900

    modified: src/test/java/java_itamae/domain/repository/owner/OwnerRepositoryTest.java
    パッケージのバージョンアップに伴う import の修正。

--- src/test/java/java_itamae/domain/repository/owner/OwnerRepositoryTest.java
+++ src/test/java/java_itamae/domain/repository/owner/OwnerRepositoryTest.java
@@ -1,7 +1,7 @@
 package java_itamae.domain.repository.owner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
 import java.io.FileNotFoundException;
