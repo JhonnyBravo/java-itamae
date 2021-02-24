76ab18c0ba071c7157b58abfd8c32ab4ddb16024
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-02-24 21:58:17 +0900

    modified: src/main/java/java_itamae/domain/model/Attribute.java
    パッケージのバージョンアップに伴う import の修正。

--- src/main/java/java_itamae/domain/model/Attribute.java
+++ src/main/java/java_itamae/domain/model/Attribute.java
@@ -2,9 +2,8 @@ package java_itamae.domain.model;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java_itamae.app.validator.NotWindows;
 
 /**
