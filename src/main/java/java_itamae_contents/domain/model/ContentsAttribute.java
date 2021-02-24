fcbcc48dd84b735d9ffec2cb6cfcb9ee5af3eec0
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-02-25 01:05:32 +0900

    modified: src/main/java/java_itamae_contents/domain/model/ContentsAttribute.java
    パッケージのバージョンアップに伴う import の修正。

--- src/main/java/java_itamae_contents/domain/model/ContentsAttribute.java
+++ src/main/java/java_itamae_contents/domain/model/ContentsAttribute.java
@@ -2,7 +2,7 @@ package java_itamae_contents.domain.model;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 public class ContentsAttribute implements Serializable {
     private static final long serialVersionUID = 1L;
