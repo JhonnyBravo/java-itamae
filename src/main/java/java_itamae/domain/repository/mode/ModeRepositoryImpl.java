d03551bfa38af101ccc750993f875ac487fa4ed8
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:22:58 +0900

    modified: src/main/java/java_itamae/domain/repository/mode/ModeRepositoryImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/java_itamae/domain/repository/mode/ModeRepositoryImpl.java
+++ src/main/java/java_itamae/domain/repository/mode/ModeRepositoryImpl.java
@@ -7,54 +7,51 @@ import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModeRepositoryImpl implements ModeRepository {
-    private final Logger logger;
+  private final Logger logger;
 
-    public ModeRepositoryImpl() {
-        logger = LoggerFactory.getLogger(this.getClass());
+  public ModeRepositoryImpl() {
+    logger = LoggerFactory.getLogger(this.getClass());
+  }
+
+  @Override
+  public Set<PosixFilePermission> createMode(String mode) throws Exception {
+    final String converted = mode.replaceAll("0", "---").replaceAll("1", "--x")
+        .replaceAll("2", "-w-").replaceAll("3", "-wx").replaceAll("4", "r--").replaceAll("5", "r-x")
+        .replaceAll("6", "rw-").replaceAll("7", "rwx");
+    return PosixFilePermissions.fromString(converted);
+  }
+
+  @Override
+  public Set<PosixFilePermission> getMode(String path) throws Exception {
+    final Path p = FileSystems.getDefault().getPath(path);
+
+    if (!p.toFile().exists()) {
+      throw new FileNotFoundException(path + " が見つかりません。");
     }
 
-    @Override
-    public Set<PosixFilePermission> createMode(String mode) throws Exception {
-        final String converted = mode.replaceAll("0", "---").replaceAll("1", "--x").replaceAll("2", "-w-")
-                .replaceAll("3", "-wx").replaceAll("4", "r--").replaceAll("5", "r-x").replaceAll("6", "rw-")
-                .replaceAll("7", "rwx");
-        final Set<PosixFilePermission> permission = PosixFilePermissions.fromString(converted);
-        return permission;
+    return Files.getPosixFilePermissions(p);
+  }
+
+  @Override
+  public boolean setMode(String path, String mode) throws Exception {
+    boolean status = false;
+
+    final Set<PosixFilePermission> curPermission = getMode(path);
+    final Set<PosixFilePermission> newPermission = createMode(mode);
+
+    if (curPermission.equals(newPermission)) {
+      return status;
     }
 
-    @Override
-    public Set<PosixFilePermission> getMode(String path) throws Exception {
-        final Path p = FileSystems.getDefault().getPath(path);
+    logger.info("パーミッションを変更しています......");
+    final Path p = FileSystems.getDefault().getPath(path);
+    Files.setPosixFilePermissions(p, newPermission);
 
-        if (!p.toFile().exists()) {
-            throw new FileNotFoundException(path + " が見つかりません。");
-        }
-
-        final Set<PosixFilePermission> permission = Files.getPosixFilePermissions(p);
-        return permission;
-    }
-
-    @Override
-    public boolean setMode(String path, String mode) throws Exception {
-        boolean status = false;
-
-        final Set<PosixFilePermission> curPermission = getMode(path);
-        final Set<PosixFilePermission> newPermission = createMode(mode);
-
-        if (curPermission.equals(newPermission)) {
-            return status;
-        }
-
-        logger.info("パーミッションを変更しています......");
-        final Path p = FileSystems.getDefault().getPath(path);
-        Files.setPosixFilePermissions(p, newPermission);
-
-        status = true;
-        return status;
-    }
+    status = true;
+    return status;
+  }
 }
