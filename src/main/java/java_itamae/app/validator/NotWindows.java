fce5af291d6a790af26df96afa6bfa459f59bbd5
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:32:09 +0900

    modified: src/main/java/java_itamae/app/validator/NotWindows.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae/app/validator/NotWindows.java
+++ src/main/java/java_itamae/app/validator/NotWindows.java
@@ -1,32 +1,33 @@
 package java_itamae.app.validator;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-
+/**
+ * OS が Windows であるかどうかを確認する。
+ */
 @Documented
 @Retention(RUNTIME)
-@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
-        ElementType.PARAMETER, ElementType.TYPE_USE })
-@Constraint(validatedBy = { NotWindowsValidator.class })
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
+    ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Constraint(validatedBy = {NotWindowsValidator.class})
 public @interface NotWindows {
-    String message() default "{java_itamae.app.NotWindows.message}";
+  String message() default "{java_itamae.app.NotWindows.message}";
 
-    Class<?>[] groups() default {};
+  Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default {};
+  Class<? extends Payload>[] payload() default {};
 
-    @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
-            ElementType.PARAMETER, ElementType.TYPE_USE })
-    @Retention(RUNTIME)
-    @Documented
-    @interface List {
-        NotWindows[] value();
-    }
+  @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
+      ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+  @Retention(RUNTIME)
+  @Documented
+  @interface List {
+    NotWindows[] value();
+  }
 }
