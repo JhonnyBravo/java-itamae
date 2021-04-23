11d2a2eed291802242730bcf5d075ba0a43e0a48
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:20:31 +0900

    modified: src/main/java/java_itamae/domain/repository/group/GroupRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae/domain/repository/group/GroupRepository.java
+++ src/main/java/java_itamae/domain/repository/group/GroupRepository.java
@@ -6,31 +6,35 @@ import java.nio.file.attribute.GroupPrincipal;
  * リソースのグループ所有者を管理する。
  */
 public interface GroupRepository {
-    /**
-     * @param group 新しいグループ所有者として設定するグループ名を指定する。
-     * @return group GroupPrincipal を生成して返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public GroupPrincipal createGroup(String group) throws Exception;
+  /**
+   * {@link GroupPrincipal} を生成する。
+   *
+   * @param group 新しいグループ所有者として設定するグループ名を指定する。
+   * @return group {@link GroupPrincipal} を生成して返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  GroupPrincipal createGroup(String group) throws Exception;
 
-    /**
-     * @param path 操作対象とするリソースのパスを指定する。
-     * @return group 現在のグループ所有者として設定されている GroupPrincipal を生成して返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public GroupPrincipal getGroup(String path) throws Exception;
+  /**
+   * {@link GroupPrincipal} を取得する。
+   *
+   * @param path 操作対象とするリソースのパスを指定する。
+   * @return group 現在のグループ所有者として設定されている {@link GroupPrincipal} を生成して返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  GroupPrincipal getGroup(String path) throws Exception;
 
-    /**
-     * リソースのグループ所有者を変更する。
-     *
-     * @param path  操作対象とするリソースのパスを指定する。
-     * @param group 新しいグループ所有者として設定するグループ名を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: グループ所有者が変更されたことを表す。</li>
-     *         <li>false: グループ所有者が変更されなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean setGroup(String path, String group) throws Exception;
+  /**
+   * リソースのグループ所有者を変更する。
+   *
+   * @param path 操作対象とするリソースのパスを指定する。
+   * @param group 新しいグループ所有者として設定するグループ名を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: グループ所有者が変更されたことを表す。</li>
+   *         <li>false: グループ所有者が変更されなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean setGroup(String path, String group) throws Exception;
 }
