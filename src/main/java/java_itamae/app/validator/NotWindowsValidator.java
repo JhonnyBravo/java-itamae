7041b82797f6d97828546b868542aff39ee3920f
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:33:05 +0900

    modified: src/main/java/java_itamae/app/validator/NotWindowsValidator.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae/app/validator/NotWindowsValidator.java
+++ src/main/java/java_itamae/app/validator/NotWindowsValidator.java
@@ -3,22 +3,24 @@ package java_itamae.app.validator;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/**
+ * OS が Windows であるかどうかを確認する。
+ */
 public class NotWindowsValidator implements ConstraintValidator<NotWindows, String> {
 
-    @Override
-    public void initialize(NotWindows constraintAnnotation) {
+  @Override
+  public void initialize(NotWindows constraintAnnotation) {}
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    boolean result = true;
+
+    final String osName = System.getProperty("os.name");
+
+    if (value != null && osName.substring(0, 3).equals("Win")) {
+      result = false;
     }
 
-    @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        boolean result = true;
-
-        final String osName = System.getProperty("os.name");
-
-        if (value != null && osName.substring(0, 3).equals("Win")) {
-            result = false;
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
