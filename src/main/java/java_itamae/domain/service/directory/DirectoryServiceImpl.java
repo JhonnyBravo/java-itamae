2c014dec73f1543a10e5a1e52e8768cdbe90e5fb
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:26:59 +0900

    modified: src/main/java/java_itamae/domain/service/directory/DirectoryServiceImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae/domain/service/directory/DirectoryServiceImpl.java
+++ src/main/java/java_itamae/domain/service/directory/DirectoryServiceImpl.java
@@ -11,60 +11,60 @@ import java_itamae.domain.repository.owner.OwnerRepository;
 import java_itamae.domain.repository.owner.OwnerRepositoryImpl;
 
 public class DirectoryServiceImpl implements DirectoryService {
-    private boolean recursive;
-    private final DirectoryRepository dr;
-    private final OwnerRepository or;
-    private final GroupRepository gr;
-    private final ModeRepository mr;
+  private boolean recursive;
+  private final DirectoryRepository dr;
+  private final OwnerRepository or;
+  private final GroupRepository gr;
+  private final ModeRepository mr;
 
-    public DirectoryServiceImpl() {
-        recursive = false;
-        dr = new DirectoryRepositoryImpl();
-        or = new OwnerRepositoryImpl();
-        gr = new GroupRepositoryImpl();
-        mr = new ModeRepositoryImpl();
+  public DirectoryServiceImpl() {
+    recursive = false;
+    dr = new DirectoryRepositoryImpl();
+    or = new OwnerRepositoryImpl();
+    gr = new GroupRepositoryImpl();
+    mr = new ModeRepositoryImpl();
+  }
+
+  @Override
+  public void setRecursive(boolean recursive) {
+    this.recursive = recursive;
+  }
+
+  @Override
+  public boolean create(Attribute attr) throws Exception {
+    boolean status = false;
+
+    if (recursive) {
+      status = dr.createRecursive(attr.getPath());
+    } else {
+      status = dr.create(attr.getPath());
     }
 
-    @Override
-    public void setRecursive(boolean recursive) {
-        this.recursive = recursive;
+    if (attr.getOwner() != null) {
+      status = or.setOwner(attr.getPath(), attr.getOwner());
     }
 
-    @Override
-    public boolean create(Attribute attr) throws Exception {
-        boolean status = false;
-
-        if (recursive) {
-            status = dr.createRecursive(attr.getPath());
-        } else {
-            status = dr.create(attr.getPath());
-        }
-
-        if (attr.getOwner() != null) {
-            status = or.setOwner(attr.getPath(), attr.getOwner());
-        }
-
-        if (attr.getGroup() != null) {
-            status = gr.setGroup(attr.getPath(), attr.getGroup());
-        }
-
-        if (attr.getMode() != null) {
-            status = mr.setMode(attr.getPath(), attr.getMode());
-        }
-
-        return status;
+    if (attr.getGroup() != null) {
+      status = gr.setGroup(attr.getPath(), attr.getGroup());
     }
 
-    @Override
-    public boolean delete(Attribute attr) throws Exception {
-        boolean status = false;
-
-        if (recursive) {
-            status = dr.deleteRecursive(attr.getPath());
-        } else {
-            status = dr.delete(attr.getPath());
-        }
-
-        return status;
+    if (attr.getMode() != null) {
+      status = mr.setMode(attr.getPath(), attr.getMode());
     }
+
+    return status;
+  }
+
+  @Override
+  public boolean delete(Attribute attr) throws Exception {
+    boolean status = false;
+
+    if (recursive) {
+      status = dr.deleteRecursive(attr.getPath());
+    } else {
+      status = dr.delete(attr.getPath());
+    }
+
+    return status;
+  }
 }
