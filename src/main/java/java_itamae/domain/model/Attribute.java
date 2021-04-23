1249d8b1fea68b6b8b0b817692357d366b0e3dda
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:14:49 +0900

    modified: src/main/java/java_itamae/domain/model/Attribute.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae/domain/model/Attribute.java
+++ src/main/java/java_itamae/domain/model/Attribute.java
@@ -1,89 +1,104 @@
 package java_itamae.domain.model;
 
-import java.io.Serializable;
-
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import java.io.Serializable;
 import java_itamae.app.validator.NotWindows;
 
 /**
  * リソースの管理に使用する属性を管理する。
  */
 public class Attribute implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @NotNull
-    private String path;
-    private String owner;
-    @NotWindows
-    private String group;
-    @Pattern(regexp = "[0-7]{3}")
-    @NotWindows
-    private String mode;
+  @NotNull
+  private String path;
+  private String owner;
+  @NotWindows
+  private String group;
+  @Pattern(regexp = "[0-7]{3}")
+  @NotWindows
+  private String mode;
 
-    /**
-     * @return path 操作対象とするリソースのパスを返す。
-     */
-    public String getPath() {
-        return path;
-    }
+  /**
+   * 操作対象とするリソースのパスを返す。
+   *
+   * @return path 操作対象とするリソースのパス
+   */
+  public String getPath() {
+    return path;
+  }
 
-    /**
-     * @param path 操作対象とするリソースのパスを指定する。
-     */
-    public void setPath(String path) {
-        this.path = path;
-    }
+  /**
+   * 操作対象とするリソースのパスを設定する。
+   *
+   * @param path 操作対象とするリソースのパス
+   */
+  public void setPath(String path) {
+    this.path = path;
+  }
 
-    /**
-     * @return owner リソースの所有者を返す。
-     */
-    public String getOwner() {
-        return owner;
-    }
+  /**
+   * リソースの所有者を返す。
+   *
+   * @return owner リソースの所有者
+   */
+  public String getOwner() {
+    return owner;
+  }
 
-    /**
-     * @param owner リソースに設定する所有者を指定する。
-     */
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
+  /**
+   * リソースの所有者を設定する。
+   *
+   * @param owner リソースに設定する所有者
+   */
+  public void setOwner(String owner) {
+    this.owner = owner;
+  }
 
-    /**
-     * @return group リソースのグループ所有者を返す。
-     */
-    public String getGroup() {
-        return group;
-    }
+  /**
+   * リソースのグループ所有者を返す。
+   *
+   * @return group リソースのグループ所有者
+   */
+  public String getGroup() {
+    return group;
+  }
 
-    /**
-     * @param group リソースに設定するグループ所有者を指定する。
-     */
-    public void setGroup(String group) {
-        this.group = group;
-    }
+  /**
+   * リソースのグループ所有者を設定する。
+   *
+   * @param group リソースに設定するグループ所有者
+   */
+  public void setGroup(String group) {
+    this.group = group;
+  }
 
-    /**
-     * @return mode リソースのパーミッション値を返す。
-     */
-    public String getMode() {
-        return mode;
-    }
+  /**
+   * リソースのパーミッション値を返す。
+   *
+   * @return mode リソースのパーミッション値
+   */
+  public String getMode() {
+    return mode;
+  }
 
-    /**
-     * @param mode リソースに設定するパーミッション値を 3 桁の整数で指定する。
-     *             <ul>
-     *             <li>0: ---</li>
-     *             <li>1: --x</li>
-     *             <li>2: -w-</li>
-     *             <li>3: -wx</li>
-     *             <li>4: r--</li>
-     *             <li>5: r-x</li>
-     *             <li>6: rw-</li>
-     *             <li>7: rwx</li>
-     *             </ul>
-     */
-    public void setMode(String mode) {
-        this.mode = mode;
-    }
+  /**
+   * リソースのパーミッション設定値を設定する。
+   *
+   * @param mode リソースに設定するパーミッション値を 3 桁の整数で指定する。
+   *        <ul>
+   *        <li>0: ---</li>
+   *        <li>1: --x</li>
+   *        <li>2: -w-</li>
+   *        <li>3: -wx</li>
+   *        <li>4: r--</li>
+   *        <li>5: r-x</li>
+   *        <li>6: rw-</li>
+   *        <li>7: rwx</li>
+   *        </ul>
+   */
+  public void setMode(String mode) {
+    this.mode = mode;
+  }
 }
