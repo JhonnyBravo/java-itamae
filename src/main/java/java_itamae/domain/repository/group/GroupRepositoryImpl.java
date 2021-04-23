932eefd55409f8b5d718ef682f7f7add14f6fd6a
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:21:14 +0900

    modified: src/main/java/java_itamae/domain/repository/group/GroupRepositoryImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/java_itamae/domain/repository/group/GroupRepositoryImpl.java
+++ src/main/java/java_itamae/domain/repository/group/GroupRepositoryImpl.java
@@ -7,54 +7,52 @@ import java.nio.file.Path;
 import java.nio.file.attribute.GroupPrincipal;
 import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.UserPrincipalLookupService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GroupRepositoryImpl implements GroupRepository {
-    private final Logger logger;
+  private final Logger logger;
 
-    public GroupRepositoryImpl() {
-        logger = LoggerFactory.getLogger(this.getClass());
+  public GroupRepositoryImpl() {
+    logger = LoggerFactory.getLogger(this.getClass());
+  }
+
+  @Override
+  public GroupPrincipal createGroup(String group) throws Exception {
+    final UserPrincipalLookupService upls =
+        FileSystems.getDefault().getUserPrincipalLookupService();
+    return upls.lookupPrincipalByGroupName(group);
+  }
+
+  @Override
+  public GroupPrincipal getGroup(String path) throws Exception {
+    final Path p = FileSystems.getDefault().getPath(path);
+
+    if (!p.toFile().exists()) {
+      throw new FileNotFoundException(path + " が見つかりません。");
     }
 
-    @Override
-    public GroupPrincipal createGroup(String group) throws Exception {
-        final UserPrincipalLookupService upls = FileSystems.getDefault().getUserPrincipalLookupService();
-        final GroupPrincipal gp = upls.lookupPrincipalByGroupName(group);
-        return gp;
+    final PosixFileAttributeView pfav = Files.getFileAttributeView(p, PosixFileAttributeView.class);
+    return pfav.readAttributes().group();
+  }
+
+  @Override
+  public boolean setGroup(String path, String group) throws Exception {
+    boolean status = false;
+
+    final GroupPrincipal curGroup = getGroup(path);
+    final GroupPrincipal newGroup = createGroup(group);
+
+    if (curGroup.equals(newGroup)) {
+      return status;
     }
 
-    @Override
-    public GroupPrincipal getGroup(String path) throws Exception {
-        final Path p = FileSystems.getDefault().getPath(path);
+    logger.info("グループ所有者を変更しています......");
+    final Path p = FileSystems.getDefault().getPath(path);
+    final PosixFileAttributeView pfav = Files.getFileAttributeView(p, PosixFileAttributeView.class);
+    pfav.setGroup(newGroup);
 
-        if (!p.toFile().exists()) {
-            throw new FileNotFoundException(path + " が見つかりません。");
-        }
-
-        final PosixFileAttributeView pfav = Files.getFileAttributeView(p, PosixFileAttributeView.class);
-        final GroupPrincipal gp = pfav.readAttributes().group();
-        return gp;
-    }
-
-    @Override
-    public boolean setGroup(String path, String group) throws Exception {
-        boolean status = false;
-
-        final GroupPrincipal curGroup = getGroup(path);
-        final GroupPrincipal newGroup = createGroup(group);
-
-        if (curGroup.equals(newGroup)) {
-            return status;
-        }
-
-        logger.info("グループ所有者を変更しています......");
-        final Path p = FileSystems.getDefault().getPath(path);
-        final PosixFileAttributeView pfav = Files.getFileAttributeView(p, PosixFileAttributeView.class);
-        pfav.setGroup(newGroup);
-
-        status = true;
-        return status;
-    }
+    status = true;
+    return status;
+  }
 }
