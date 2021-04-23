a96e5d237fc9a1f91c34f0fbbff3e1c04300ea4c
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:19:27 +0900

    modified: src/main/java/java_itamae/domain/repository/file/FileRepositoryImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/java_itamae/domain/repository/file/FileRepositoryImpl.java
+++ src/main/java/java_itamae/domain/repository/file/FileRepositoryImpl.java
@@ -3,42 +3,41 @@ package java_itamae.domain.repository.file;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FileRepositoryImpl implements FileRepository {
-    private final Logger logger;
+  private final Logger logger;
 
-    public FileRepositoryImpl() {
-        logger = LoggerFactory.getLogger(this.getClass());
+  public FileRepositoryImpl() {
+    logger = LoggerFactory.getLogger(this.getClass());
+  }
+
+  @Override
+  public boolean create(String path) throws Exception {
+    boolean status = false;
+    final Path p = FileSystems.getDefault().getPath(path);
+
+    if (!p.toFile().isFile()) {
+      logger.info("{} を作成しています......", path);
+      Files.createFile(p);
+      status = true;
     }
 
-    @Override
-    public boolean create(String path) throws Exception {
-        boolean status = false;
-        final Path p = FileSystems.getDefault().getPath(path);
+    return status;
+  }
 
-        if (!p.toFile().isFile()) {
-            logger.info(path + " を作成しています......");
-            Files.createFile(p);
-            status = true;
-        }
+  @Override
+  public boolean delete(String path) throws Exception {
+    boolean status = false;
+    final Path p = FileSystems.getDefault().getPath(path);
 
-        return status;
+    if (p.toFile().isFile()) {
+      logger.info("{} を削除しています......", path);
+      Files.delete(p);
+      status = true;
     }
 
-    @Override
-    public boolean delete(String path) throws Exception {
-        boolean status = false;
-        final Path p = FileSystems.getDefault().getPath(path);
-
-        if (p.toFile().isFile()) {
-            logger.info(path + " を削除しています......");
-            Files.delete(p);
-            status = true;
-        }
-
-        return status;
-    }
+    return status;
+  }
 }
