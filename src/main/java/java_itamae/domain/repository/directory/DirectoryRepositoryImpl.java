818c30c8ada8b633dd1efa58138e9466ec22e784
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:17:00 +0900

    modified: src/main/java/java_itamae/domain/repository/directory/DirectoryRepositoryImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/java_itamae/domain/repository/directory/DirectoryRepositoryImpl.java
+++ src/main/java/java_itamae/domain/repository/directory/DirectoryRepositoryImpl.java
@@ -4,78 +4,77 @@ import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DirectoryRepositoryImpl implements DirectoryRepository {
-    private final Logger logger;
+  private final Logger logger;
 
-    public DirectoryRepositoryImpl() {
-        logger = LoggerFactory.getLogger(this.getClass());
+  public DirectoryRepositoryImpl() {
+    logger = LoggerFactory.getLogger(this.getClass());
+  }
+
+  @Override
+  public boolean create(String path) throws Exception {
+    boolean status = false;
+    final Path p = FileSystems.getDefault().getPath(path);
+
+    if (!p.toFile().isDirectory()) {
+      logger.info("{} を作成しています......", path);
+      Files.createDirectory(p);
+      status = true;
     }
 
-    @Override
-    public boolean create(String path) throws Exception {
-        boolean status = false;
-        final Path p = FileSystems.getDefault().getPath(path);
+    return status;
+  }
 
-        if (!p.toFile().isDirectory()) {
-            logger.info(path + " を作成しています......");
-            Files.createDirectory(p);
-            status = true;
+  @Override
+  public boolean createRecursive(String path) throws Exception {
+    boolean status = false;
+    final Path p = FileSystems.getDefault().getPath(path);
+
+    if (!p.toFile().isDirectory()) {
+      logger.info("{} を作成しています......", path);
+      Files.createDirectories(p);
+      status = true;
+    }
+
+    return status;
+  }
+
+  @Override
+  public boolean delete(String path) throws Exception {
+    boolean status = false;
+    final Path p = FileSystems.getDefault().getPath(path);
+
+    if (p.toFile().isDirectory()) {
+      logger.info("{} を削除しています......", path);
+      Files.delete(p);
+      status = true;
+    }
+
+    return status;
+  }
+
+  @Override
+  public boolean deleteRecursive(String path) throws Exception {
+    boolean status = false;
+    final File directory = new File(path);
+
+    if (directory.isDirectory()) {
+      for (final File file : directory.listFiles()) {
+        if (file.isDirectory()) {
+          deleteRecursive(file.getPath());
+        } else {
+          logger.info("{} を削除しています......", file.getPath());
+          file.delete();
         }
+      }
 
-        return status;
+      logger.info("{} を削除しています......", path);
+      status = directory.delete();
     }
 
-    @Override
-    public boolean createRecursive(String path) throws Exception {
-        boolean status = false;
-        final Path p = FileSystems.getDefault().getPath(path);
-
-        if (!p.toFile().isDirectory()) {
-            logger.info(path + " を作成しています......");
-            Files.createDirectories(p);
-            status = true;
-        }
-
-        return status;
-    }
-
-    @Override
-    public boolean delete(String path) throws Exception {
-        boolean status = false;
-        final Path p = FileSystems.getDefault().getPath(path);
-
-        if (p.toFile().isDirectory()) {
-            logger.info(path + " を削除しています......");
-            Files.delete(p);
-            status = true;
-        }
-
-        return status;
-    }
-
-    @Override
-    public boolean deleteRecursive(String path) throws Exception {
-        boolean status = false;
-        final File directory = new File(path);
-
-        if (directory.isDirectory()) {
-            for (final File file : directory.listFiles()) {
-                if (file.isDirectory()) {
-                    deleteRecursive(file.getPath());
-                } else {
-                    logger.info(file.getPath() + " を削除しています......");
-                    file.delete();
-                }
-            }
-
-            logger.info(path + " を削除しています......");
-            status = directory.delete();
-        }
-
-        return status;
-    }
+    return status;
+  }
 }
