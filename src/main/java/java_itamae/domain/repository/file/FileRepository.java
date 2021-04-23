ec2aa22ea2962babfec6867045809790040c9c44
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-24 01:18:40 +0900

    modified: src/main/java/java_itamae/domain/repository/file/FileRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae/domain/repository/file/FileRepository.java
+++ src/main/java/java_itamae/domain/repository/file/FileRepository.java
@@ -1,29 +1,32 @@
 package java_itamae.domain.repository.file;
 
+/**
+ * ファイルの操作を管理する。
+ */
 public interface FileRepository {
-    /**
-     * ファイルを作成する。
-     * 
-     * @param path 操作対象とするファイルのパスを指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: ファイルが作成されたことを表す。</li>
-     *         <li>false: ファイルが作成されなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean create(String path) throws Exception;
+  /**
+   * ファイルを作成する。
+   *
+   * @param path 操作対象とするファイルのパスを指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: ファイルが作成されたことを表す。</li>
+   *         <li>false: ファイルが作成されなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean create(String path) throws Exception;
 
-    /**
-     * ファイルを削除する。
-     * 
-     * @param path 操作対象とするファイルのパスを指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: ファイルが削除されたことを表す。</li>
-     *         <li>false: ファイルが削除されなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean delete(String path) throws Exception;
+  /**
+   * ファイルを削除する。
+   *
+   * @param path 操作対象とするファイルのパスを指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: ファイルが削除されたことを表す。</li>
+   *         <li>false: ファイルが削除されなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean delete(String path) throws Exception;
 }
