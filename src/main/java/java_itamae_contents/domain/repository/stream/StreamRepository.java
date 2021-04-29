c0abdf1426d8becb94e28527b2a95076dff08078
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-30 01:16:55 +0900

    modified: src/main/java/java_itamae_contents/domain/repository/stream/StreamRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae_contents/domain/repository/stream/StreamRepository.java
+++ src/main/java/java_itamae_contents/domain/repository/stream/StreamRepository.java
@@ -2,24 +2,27 @@ package java_itamae_contents.domain.repository.stream;
 
 import java.io.Reader;
 import java.io.Writer;
-
 import java_itamae_contents.domain.model.ContentsAttribute;
 
 /**
  * テキストファイルの読み書きに使用するストリームを取得する。
  */
 public interface StreamRepository {
-    /**
-     * @param attr 操作対象とする ContentsAttribute を指定する。
-     * @return reader ファイルストリームを読取モードで開いて返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public Reader getReader(ContentsAttribute attr) throws Exception;
+  /**
+   * ファイルストリームを読取モードで開いて返す。
+   *
+   * @param attr 操作対象とする {@link ContentsAttribute} を指定する。
+   * @return reader {@link Reader} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  Reader getReader(ContentsAttribute attr) throws Exception;
 
-    /**
-     * @param attr 操作対象とする ContentsAttribute を指定する。
-     * @return writer ファイルストリームを書込モードで開いて返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public Writer getWriter(ContentsAttribute attr) throws Exception;
+  /**
+   * ファイルストリームを書込モードで開いて返す。
+   *
+   * @param attr 操作対象とする {@link ContentsAttribute} を指定する。
+   * @return writer {@link Writer} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  Writer getWriter(ContentsAttribute attr) throws Exception;
 }
