e2e12ac9b07fda4c210296a7cc275b7bd77f2e1b
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-30 01:18:53 +0900

    modified: src/main/java/java_itamae_contents/domain/repository/stream/StreamRepositoryImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae_contents/domain/repository/stream/StreamRepositoryImpl.java
+++ src/main/java/java_itamae_contents/domain/repository/stream/StreamRepositoryImpl.java
@@ -8,40 +8,41 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
-
 import java_itamae_contents.domain.model.ContentsAttribute;
 
 public class StreamRepositoryImpl implements StreamRepository {
 
-    @Override
-    public Reader getReader(ContentsAttribute attr) throws Exception {
-        final File file = new File(attr.getPath());
+  @Override
+  public Reader getReader(ContentsAttribute attr) throws Exception {
+    final File file = new File(attr.getPath());
 
-        if (!file.isFile()) {
-            throw new FileNotFoundException(attr.getPath() + " が見つかりません。");
-        }
-
-        if (attr.getEncoding() == null) {
-            attr.setEncoding(System.getProperty("file.encoding"));
-        }
-
-        final Reader reader = new InputStreamReader(new FileInputStream(attr.getPath()), attr.getEncoding());
-        return reader;
+    if (!file.isFile()) {
+      throw new FileNotFoundException(attr.getPath() + " が見つかりません。");
     }
 
-    @Override
-    public Writer getWriter(ContentsAttribute attr) throws Exception {
-        final File file = new File(attr.getPath());
-
-        if (!file.isFile()) {
-            throw new FileNotFoundException(attr.getPath() + " が見つかりません。");
-        }
-
-        if (attr.getEncoding() == null) {
-            attr.setEncoding(System.getProperty("file.encoding"));
-        }
-
-        final Writer writer = new OutputStreamWriter(new FileOutputStream(attr.getPath()), attr.getEncoding());
-        return writer;
+    if (attr.getEncoding() == null) {
+      attr.setEncoding(System.getProperty("file.encoding"));
     }
+
+    final Reader reader =
+        new InputStreamReader(new FileInputStream(attr.getPath()), attr.getEncoding());
+    return reader;
+  }
+
+  @Override
+  public Writer getWriter(ContentsAttribute attr) throws Exception {
+    final File file = new File(attr.getPath());
+
+    if (!file.isFile()) {
+      throw new FileNotFoundException(attr.getPath() + " が見つかりません。");
+    }
+
+    if (attr.getEncoding() == null) {
+      attr.setEncoding(System.getProperty("file.encoding"));
+    }
+
+    final Writer writer =
+        new OutputStreamWriter(new FileOutputStream(attr.getPath()), attr.getEncoding());
+    return writer;
+  }
 }
