65259f555d0767619c13cace15e7018b978fb32a
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-30 01:20:22 +0900

    modified: src/main/java/java_itamae_contents/domain/service/contents/ContentsService.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae_contents/domain/service/contents/ContentsService.java
+++ src/main/java/java_itamae_contents/domain/service/contents/ContentsService.java
@@ -2,48 +2,53 @@ package java_itamae_contents.domain.service.contents;
 
 import java.util.List;
 
+/**
+ * ファイルの読み書きを操作する。
+ */
 public interface ContentsService {
-    /**
-     * @return contents ファイルを読込み、 List に変換して返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public List<String> getContents() throws Exception;
+  /**
+   * ファイルを読込み、 {@link List} に変換して返す。
+   *
+   * @return contents {@link String} の {@link List}
+   * @throws Exception {@link java.lang.Exception}
+   */
+  List<String> getContents() throws Exception;
 
-    /**
-     * ファイルを上書きする。
-     *
-     * @param content 書込み対象とする文字列を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 上書きに成功したことを表す。</li>
-     *         <li>false: 上書きを実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean setContent(String content) throws Exception;
+  /**
+   * ファイルを上書きする。
+   *
+   * @param content 書込み対象とする文字列を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 上書きに成功したことを表す。</li>
+   *         <li>false: 上書きを実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean updateContent(String content) throws Exception;
 
-    /**
-     * ファイル末尾へ文字列を追記する。
-     *
-     * @param content 書込み対象とする文字列を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 追記に成功したことを表す。</li>
-     *         <li>false: 追記を実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean appendContent(String content) throws Exception;
+  /**
+   * ファイル末尾へ文字列を追記する。
+   *
+   * @param content 書込み対象とする文字列を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 追記に成功したことを表す。</li>
+   *         <li>false: 追記を実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean appendContent(String content) throws Exception;
 
-    /**
-     * ファイルを空にする。
-     *
-     * @return status
-     *         <ul>
-     *         <li>true: ファイルの内容削除に成功したことを表す。</li>
-     *         <li>false: ファイルの内容削除を実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean deleteContents() throws Exception;
+  /**
+   * ファイルを空にする。
+   *
+   * @return status
+   *         <ul>
+   *         <li>true: ファイルの内容削除に成功したことを表す。</li>
+   *         <li>false: ファイルの内容削除を実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean deleteContents() throws Exception;
 }
