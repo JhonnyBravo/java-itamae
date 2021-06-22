488d72dc81477a328a4a5fb6f25c19ceaffab5b3
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-06-23 02:13:28 +0900

    renamed: src/test/java/java_itamae/domain/repository/mode/{ModeRepositoryTest.java -> TestModeRepository.java}
    CDI Unit の導入に伴い、以下のクラスに分割。
    
    * DirectoryMode
    * FileMode

---
+++ src/test/java/java_itamae/domain/repository/mode/TestModeRepository.java
@@ -0,0 +1,10 @@
+package java_itamae.domain.repository.mode;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({DirectoryMode.class, FileMode.class})
+public class TestModeRepository {
+}
