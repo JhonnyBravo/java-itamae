2af2844eba3121eb56a448f6eab3270a7a30da22
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-06-23 02:17:07 +0900

    renamed: src/test/java/java_itamae/domain/repository/owner/{OwnerRepositoryTest.java -> TestOwnerRepository.java}
    CDI Unit の導入に伴い、以下のクラスに分割。
    
    * DirectoryOwner
    * FileOwner

---
+++ src/test/java/java_itamae/domain/repository/owner/TestOwnerRepository.java
@@ -0,0 +1,10 @@
+package java_itamae.domain.repository.owner;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({DirectoryOwner.class, FileOwner.class})
+public class TestOwnerRepository {
+}
