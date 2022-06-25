ed523ee3b9cbef164f5921db216c6ff4bc9a32fa
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-06-25 17:24:45 +0900

    renamed: src/main/java/java_itamae_contents/{app/validator/IsValidContentsAttribute.java -> domain/model/contents/IsValidContentsModel.java}

--- src/main/java/java_itamae_contents/domain/model/contents/IsValidContentsModel.java
+++ src/main/java/java_itamae_contents/domain/model/contents/IsValidContentsModel.java
@@ -1,22 +1,23 @@
-package java_itamae_contents.app.validator;
+package java_itamae_contents.domain.model.contents;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+
 import java.util.Set;
 import java.util.function.Predicate;
-import java_itamae_contents.domain.model.ContentsAttribute;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * {@link ContentsAttribute} のバリデーションチェックを実行する。
+ * {@link ContentsModel} のバリデーションチェックを実行する。
  */
-public class IsValidContentsAttribute implements Predicate<ContentsAttribute> {
+public class IsValidContentsModel implements Predicate<ContentsModel> {
   /**
-   * {@link ContentsAttribute} のバリデーションチェックを実行する。
+   * {@link ContentsModel} のバリデーションチェックを実行する。
    *
-   * @param attr 判定対象とする {@link ContentsAttribute} を指定する。
+   * @param attr 判定対象とする {@link ContentsModel} を指定する。
    * @return isValid
    *         <ul>
    *         <li>true: エラーが無いことを表す。</li>
@@ -24,10 +25,10 @@ public class IsValidContentsAttribute implements Predicate<ContentsAttribute> {
    *         </ul>
    */
   @Override
-  public boolean test(ContentsAttribute attr) {
+  public boolean test(ContentsModel attr) {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-    final Set<ConstraintViolation<ContentsAttribute>> resultSet = validator.validate(attr);
+    final Set<ConstraintViolation<ContentsModel>> resultSet = validator.validate(attr);
 
     if (resultSet.size() > 0) {
       resultSet.stream().forEach(error -> {
