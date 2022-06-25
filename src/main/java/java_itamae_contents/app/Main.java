18ffa924d4479b73d044f952dcd1486cdcc23ece
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-06-25 16:51:25 +0900

    modified: src/main/java/java_itamae_contents/app/Main.java
    プロパティファイルを読込み、ファイルの設定値に従って動作を実行するように修正。

--- src/main/java/java_itamae_contents/app/Main.java
+++ src/main/java/java_itamae_contents/app/Main.java
@@ -1,52 +1,40 @@
 package java_itamae_contents.app;
 
+import java.util.List;
+import java.util.Map;
+
 import gnu.getopt.Getopt;
 import gnu.getopt.LongOpt;
-import java_itamae_contents.app.contents.AppendContent;
-import java_itamae_contents.app.contents.DeleteContents;
-import java_itamae_contents.app.contents.GetContents;
-import java_itamae_contents.app.contents.SetContent;
-import java_itamae_contents.app.validator.IsValidContentsAttribute;
-import java_itamae_contents.domain.model.ContentsAttribute;
+import java_itamae_contents.app.template.DeleteContents;
+import java_itamae_contents.app.template.GetContents;
+import java_itamae_contents.app.template.GetProperties;
+import java_itamae_contents.app.template.UpdateContents;
+import java_itamae_contents.domain.model.contents.ContentsModel;
+import java_itamae_contents.domain.model.contents.IsValidContentsModel;
+import java_itamae_contents.domain.model.template.TemplateResourceModel;
+import java_itamae_contents.domain.model.template.TemplateResourceModelValidator;
 
-/**
- * テキストファイルの読み書きを実行する。
- */
+/** プロパティファイルを読込み、ファイルの設定値に従って動作を実行する。 */
 public class Main {
   /**
-   * テキストファイルの読み書きを実行する。
+   * プロパティファイルを読込み、動作を実行する。
    *
    * @param args
-   *        <ul>
-   *        <li>--path, -p &lt;path&gt;: 操作対象とするテキストファイルのパスを指定する。</li>
-   *        <li>--encoding, -e &lt;encoding&gt;: 文字エンコーディングを指定する。</li>
-   *        <li>--set-content, -s &lt;content&gt;: テキストファイルを上書きする。</li>
-   *        <li>--append-content, -a &lt;content&gt;: テキストファイル末尾へ文字列を追記する。</li>
-   *        <li>--delete-content, -d: テキストファイルの内容を削除して空にする。</li>
-   *        <li>--get-content, -g: テキストファイルの内容を読込んで標準出力へ出力する。。</li>
-   *        </ul>
+   *     <ul>
+   *       <li>--path, -p &lt;path&gt;: 読込み対象とするプロパティファイルのパスを指定する。
+   *       <li>--encoding, -e &lt;encoding&gt;: 文字エンコーディングを指定する。
+   *     </ul>
    */
   public static void main(String[] args) {
-    final LongOpt[] longopts = new LongOpt[6];
+    final LongOpt[] longopts = new LongOpt[2];
     longopts[0] = new LongOpt("path", LongOpt.REQUIRED_ARGUMENT, null, 'p');
     longopts[1] = new LongOpt("encoding", LongOpt.REQUIRED_ARGUMENT, null, 'e');
-    longopts[2] = new LongOpt("set-content", LongOpt.REQUIRED_ARGUMENT, null, 's');
-    longopts[3] = new LongOpt("append-content", LongOpt.REQUIRED_ARGUMENT, null, 'a');
-    longopts[4] = new LongOpt("get-content", LongOpt.NO_ARGUMENT, null, 'g');
-    longopts[5] = new LongOpt("delete-content", LongOpt.NO_ARGUMENT, null, 'd');
 
-    final Getopt options = new Getopt("Main", args, "p:e:s:a:gd", longopts);
+    final Getopt options = new Getopt("Main", args, "p:e:", longopts);
 
-    final ContentsAttribute attr = new ContentsAttribute();
+    final ContentsModel cliArgs = new ContentsModel();
 
     int c;
-    int setFlag = 0;
-    int appendFlag = 0;
-    int getFlag = 0;
-    int deleteFlag = 0;
-
-    String content = null;
-
     int status = 0;
     final Usage usage = new Usage();
 
@@ -57,24 +45,10 @@ public class Main {
     while ((c = options.getopt()) != -1) {
       switch (c) {
         case 'p':
-          attr.setPath(options.getOptarg());
+          cliArgs.setPath(options.getOptarg());
           break;
         case 'e':
-          attr.setEncoding(options.getOptarg());
-          break;
-        case 's':
-          content = options.getOptarg();
-          setFlag = 1;
-          break;
-        case 'a':
-          content = options.getOptarg();
-          appendFlag = 1;
-          break;
-        case 'g':
-          getFlag = 1;
-          break;
-        case 'd':
-          deleteFlag = 1;
+          cliArgs.setEncoding(options.getOptarg());
           break;
         default:
           usage.run();
@@ -83,18 +57,60 @@ public class Main {
       }
     }
 
-    if (new IsValidContentsAttribute().test(attr)) {
-      if (appendFlag == 1) {
-        status = new AppendContent().apply(attr, content);
-      } else if (setFlag == 1) {
-        status = new SetContent().apply(attr, content);
-      } else if (deleteFlag == 1) {
-        status = new DeleteContents().apply(attr);
-      } else if (getFlag == 1) {
-        status = new GetContents().apply(attr);
-      }
-    } else {
+    if (!new IsValidContentsModel().test(cliArgs)) {
       status = 1;
+      System.exit(status);
+    }
+
+    // プロパティファイルからキーと値を読み込む。
+    final GetProperties getProperties = new GetProperties();
+    final Map<String, String> properties = getProperties.apply(cliArgs);
+
+    // resource_name を取得し、実行するリソースを判定する。
+    final String resourceName = properties.get("resource_name");
+    final String action = properties.get("action");
+
+    if (resourceName.equals("template")) {
+      // 各プロパティの値をモデルへ設定する。
+      final TemplateResourceModel resourceModel = new TemplateResourceModel();
+      resourceModel.setAction(properties.get("action"));
+      resourceModel.setPath(properties.get("path"));
+      resourceModel.setSource(properties.get("source"));
+      resourceModel.setEncoding(properties.get("encoding"));
+
+      // バリデーションチェックを実行する。
+      final TemplateResourceModelValidator validator = new TemplateResourceModelValidator();
+
+      if (!validator.test(resourceModel)) {
+        status = 1;
+        System.exit(status);
+      }
+
+      // 動作を実行する。
+      if (action.equals("create")) {
+        // 読込み対象とするテキストファイル
+        final ContentsModel sourceAttr = new ContentsModel();
+        sourceAttr.setPath(resourceModel.getSource());
+        sourceAttr.setEncoding(resourceModel.getEncoding());
+
+        final GetContents getContents = new GetContents();
+        final List<String> contents = getContents.apply(sourceAttr);
+
+        // 書込み対象とするテキストファイル
+        final ContentsModel targetAttr = new ContentsModel();
+        targetAttr.setPath(resourceModel.getPath());
+        targetAttr.setEncoding(resourceModel.getEncoding());
+
+        final UpdateContents updateContents = new UpdateContents();
+        status = updateContents.apply(targetAttr, contents);
+      } else if (action.equals("delete")) {
+        final ContentsModel targetAttr = new ContentsModel();
+        targetAttr.setPath(resourceModel.getPath());
+        targetAttr.setEncoding(resourceModel.getEncoding());
+
+        final DeleteContents deleteContents = new DeleteContents();
+        status = deleteContents.apply(targetAttr);
+      }
     }
 
     System.exit(status);
