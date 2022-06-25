8cb8231739f8f1def59eaf7b6cdc7adcce7d4fab
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-06-25 16:48:58 +0900

    modified: src/main/java/java_itamae_contents/app/Usage.java
    実行動作の変更に伴う説明文の修正。

--- src/main/java/java_itamae_contents/app/Usage.java
+++ src/main/java/java_itamae_contents/app/Usage.java
@@ -3,26 +3,20 @@ package java_itamae_contents.app;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * CLI コマンドの使用法を表示する。
- */
+/** CLI コマンドの使用法を表示する。 */
 public class Usage implements Runnable {
-  /**
-   * CLI コマンドの使用法を表示する。
-   */
+  /** CLI コマンドの使用法を表示する。 */
   @Override
   public void run() {
     final List<String> optionList = new ArrayList<>();
 
-    optionList.add("--path, -p <path>: 指定したテキストファイルに対して操作を実行します。");
-    optionList.add("--encoding, -e <encoding>: 文字エンコーディングを指定します。");
-    optionList.add("--set-content, -s <content>: テキストファイルを上書きします。");
-    optionList.add("--append-content, -a <content>: テキストファイルの末尾へ文字列を追記します。");
-    optionList.add("--get-content, -g: テキストファイルの内容を読込んで標準出力へ出力します。");
-    optionList.add("--delete-content, -d: テキストファイルの内容を削除して空にします。");
+    optionList.add("プロパティファイルを読込み、ファイルの設定値に従って動作を実行します。");
+    optionList.add("--path, -p <path>: 読込み対象とするプロパティのパスを指定します。");
+    optionList.add("--encoding, -e <encoding>: 読込み時に使用する文字エンコーディングを指定します。");
 
-    optionList.forEach(option -> {
-      System.out.println(option);
-    });
+    optionList.forEach(
+        option -> {
+          System.out.println(option);
+        });
   }
 }
