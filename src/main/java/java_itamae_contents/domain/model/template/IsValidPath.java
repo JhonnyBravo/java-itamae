4a11e84d854a7b494fac35dccf0b305144a619bf
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-06-26 16:55:08 +0900

    new file: src/main/java/java_itamae_contents/domain/model/template/{IsValidPath.java, IsValidPathValidator.java}

---
+++ src/main/java/java_itamae_contents/domain/model/template/IsValidPath.java
@@ -0,0 +1,22 @@
+package java_itamae_contents.domain.model.template;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target(TYPE)
+@Constraint(validatedBy = {IsValidPathValidator.class})
+public @interface IsValidPath {
+  String message() default "{java_itamae_contents.domain.model.template.IsValidPath}";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
