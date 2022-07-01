2e246b089606331997ee28fbe490410ca65be76e
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-02 01:13:11 +0900

    modified: src/test/java/java_itamae/domain/repository/owner/OwnerRepositoryTest.java
    パッケージ構成の変更に伴うコード修正。

--- src/test/java/java_itamae/domain/repository/owner/OwnerRepositoryTest.java
+++ src/test/java/java_itamae/domain/repository/owner/OwnerRepositoryTest.java
@@ -13,125 +13,129 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-import java_itamae_contents.domain.model.ContentsAttribute;
-import java_itamae_properties.domain.service.properties.PropertiesService;
-import java_itamae_properties.domain.service.properties.PropertiesServiceImpl;
+import java_itamae.domain.model.contents.ContentsModel;
+import java_itamae.domain.service.properties.PropertiesService;
+import java_itamae.domain.service.properties.PropertiesServiceImpl;
 
 @RunWith(Enclosed.class)
 public class OwnerRepositoryTest {
-    public static class ディレクトリの所有者を変更する場合 {
-        private OwnerRepository or;
-        private File directory;
-        private String owner;
+  public static class ディレクトリの所有者を変更する場合 {
+    private OwnerRepository or;
+    private File directory;
+    private String owner;
 
-        @Before
-        public void setUp() throws Exception {
-            or = new OwnerRepositoryImpl();
-            directory = new File("test_dir");
-            directory.mkdir();
+    @Before
+    public void setUp() throws Exception {
+      or = new OwnerRepositoryImpl();
+      directory = new File("test_dir");
+      directory.mkdir();
 
-            final ContentsAttribute attr = new ContentsAttribute();
-            attr.setPath("src/test/resources/test.properties");
-            final PropertiesService ps = new PropertiesServiceImpl(attr);
+      final ContentsModel attr = new ContentsModel();
+      attr.setPath("src/test/resources/test.properties");
+      final PropertiesService ps = new PropertiesServiceImpl(attr);
 
-            owner = ps.getProperty("owner");
-        }
-
-        @After
-        public void tearDown() throws Exception {
-            directory.delete();
-        }
-
-        @Test
-        public void ディレクトリ所有者の変更ができて終了ステータスがtrueであること() throws Exception {
-            final boolean status = or.updateOwner("test_dir", owner);
-            assertThat(status, is(true));
-        }
-
-        @Test
-        public void 新しく設定するディレクトリ所有者のユーザ名が現在設定されているディレクトリ所有者のユーザ名と同一である場合に終了ステータスがfalseであること() throws Exception {
-            or.updateOwner("test_dir", owner);
-            final boolean status = or.updateOwner("test_dir", owner);
-
-            assertThat(status, is(false));
-        }
-
-        @Test(expected = UserPrincipalNotFoundException.class)
-        public void 新しく設定するディレクトリ所有者として存在しないユーザの名前を指定した場合にUserPrincipalNotFoundExceptionが送出されること() throws Exception {
-            try {
-                or.updateOwner("test_dir", "NotExist");
-            } catch (final Exception e) {
-                System.err.println(e);
-                throw e;
-            }
-        }
-
-        @Test(expected = FileNotFoundException.class)
-        public void 存在しないディレクトリの所有者を変更しようとした場合にFileNotFoundExceptionが送出されること() throws Exception {
-            try {
-                or.updateOwner("NotExist", owner);
-            } catch (final Exception e) {
-                System.err.println(e);
-                throw e;
-            }
-        }
+      owner = ps.getProperty("owner");
     }
 
-    public static class ファイルの所有者を変更する場合 {
-        private OwnerRepository or;
-        private File file;
-        private String owner;
-
-        @Before
-        public void setUp() throws Exception {
-            or = new OwnerRepositoryImpl();
-            file = new File("test.txt");
-            file.mkdir();
-
-            final ContentsAttribute attr = new ContentsAttribute();
-            attr.setPath("src/test/resources/test.properties");
-            final PropertiesService ps = new PropertiesServiceImpl(attr);
-
-            owner = ps.getProperty("owner");
-        }
-
-        @After
-        public void tearDown() throws Exception {
-            file.delete();
-        }
-
-        @Test
-        public void ファイル所有者の変更ができて終了ステータスがtrueであること() throws Exception {
-            final boolean status = or.updateOwner("test.txt", owner);
-            assertThat(status, is(true));
-        }
-
-        @Test
-        public void 新しく設定するファイル所有者のユーザ名が現在設定されているファイル所有者のユーザ名と同一である場合に終了ステータスがfalseであること() throws Exception {
-            or.updateOwner("test.txt", owner);
-            final boolean status = or.updateOwner("test.txt", owner);
-
-            assertThat(status, is(false));
-        }
-
-        @Test(expected = UserPrincipalNotFoundException.class)
-        public void 新しく設定するファイル所有者として存在しないユーザの名前を指定した場合にUserPrincipalNotFoundExceptionが送出されること() throws Exception {
-            try {
-                or.updateOwner("test.txt", "NotExist");
-            } catch (final Exception e) {
-                System.err.println(e);
-                throw e;
-            }
-        }
-
-        @Test(expected = FileNotFoundException.class)
-        public void 存在しないファイルの所有者を変更しようとした場合にFileNotFoundExceptionが送出されること() throws Exception {
-            try {
-                or.updateOwner("NotExist", owner);
-            } catch (final Exception e) {
-                System.err.println(e);
-                throw e;
-            }
-        }
+    @After
+    public void tearDown() throws Exception {
+      directory.delete();
     }
+
+    @Test
+    public void ディレクトリ所有者の変更ができて終了ステータスがtrueであること() throws Exception {
+      final boolean status = or.updateOwner("test_dir", owner);
+      assertThat(status, is(true));
+    }
+
+    @Test
+    public void 新しく設定するディレクトリ所有者のユーザ名が現在設定されているディレクトリ所有者のユーザ名と同一である場合に終了ステータスがfalseであること()
+        throws Exception {
+      or.updateOwner("test_dir", owner);
+      final boolean status = or.updateOwner("test_dir", owner);
+
+      assertThat(status, is(false));
+    }
+
+    @Test(expected = UserPrincipalNotFoundException.class)
+    public void 新しく設定するディレクトリ所有者として存在しないユーザの名前を指定した場合にUserPrincipalNotFoundExceptionが送出されること()
+        throws Exception {
+      try {
+        or.updateOwner("test_dir", "NotExist");
+      } catch (final Exception e) {
+        System.err.println(e);
+        throw e;
+      }
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void 存在しないディレクトリの所有者を変更しようとした場合にFileNotFoundExceptionが送出されること() throws Exception {
+      try {
+        or.updateOwner("NotExist", owner);
+      } catch (final Exception e) {
+        System.err.println(e);
+        throw e;
+      }
+    }
+  }
+
+  public static class ファイルの所有者を変更する場合 {
+    private OwnerRepository or;
+    private File file;
+    private String owner;
+
+    @Before
+    public void setUp() throws Exception {
+      or = new OwnerRepositoryImpl();
+      file = new File("test.txt");
+      file.mkdir();
+
+      final ContentsModel attr = new ContentsModel();
+      attr.setPath("src/test/resources/test.properties");
+      final PropertiesService ps = new PropertiesServiceImpl(attr);
+
+      owner = ps.getProperty("owner");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+      file.delete();
+    }
+
+    @Test
+    public void ファイル所有者の変更ができて終了ステータスがtrueであること() throws Exception {
+      final boolean status = or.updateOwner("test.txt", owner);
+      assertThat(status, is(true));
+    }
+
+    @Test
+    public void 新しく設定するファイル所有者のユーザ名が現在設定されているファイル所有者のユーザ名と同一である場合に終了ステータスがfalseであること()
+        throws Exception {
+      or.updateOwner("test.txt", owner);
+      final boolean status = or.updateOwner("test.txt", owner);
+
+      assertThat(status, is(false));
+    }
+
+    @Test(expected = UserPrincipalNotFoundException.class)
+    public void 新しく設定するファイル所有者として存在しないユーザの名前を指定した場合にUserPrincipalNotFoundExceptionが送出されること()
+        throws Exception {
+      try {
+        or.updateOwner("test.txt", "NotExist");
+      } catch (final Exception e) {
+        System.err.println(e);
+        throw e;
+      }
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void 存在しないファイルの所有者を変更しようとした場合にFileNotFoundExceptionが送出されること() throws Exception {
+      try {
+        or.updateOwner("NotExist", owner);
+      } catch (final Exception e) {
+        System.err.println(e);
+        throw e;
+      }
+    }
+  }
 }
