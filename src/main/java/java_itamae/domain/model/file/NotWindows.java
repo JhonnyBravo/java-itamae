9eadb5a690eb4347825031ea1f28a99f8629389a
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-05 00:42:08 +0900

    renamed: src/main/java/java_itamae/{app/validator -> domain/model/file}/NotWindows.java

---
+++ src/main/java/java_itamae/domain/model/file/NotWindows.java
@@ -0,0 +1,45 @@
+package java_itamae.domain.model.file;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+/** OS が Windows であるかどうかを確認する。 */
+@Documented
+@Retention(RUNTIME)
+@Target({
+  ElementType.METHOD,
+  ElementType.FIELD,
+  ElementType.ANNOTATION_TYPE,
+  ElementType.CONSTRUCTOR,
+  ElementType.PARAMETER,
+  ElementType.TYPE_USE
+})
+@Constraint(validatedBy = {NotWindowsValidator.class})
+public @interface NotWindows {
+  String message() default "{java_itamae.domain.model.file.NotWindows}";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+
+  @Target({
+    ElementType.METHOD,
+    ElementType.FIELD,
+    ElementType.ANNOTATION_TYPE,
+    ElementType.CONSTRUCTOR,
+    ElementType.PARAMETER,
+    ElementType.TYPE_USE
+  })
+  @Retention(RUNTIME)
+  @Documented
+  @interface List {
+    NotWindows[] value();
+  }
+}
