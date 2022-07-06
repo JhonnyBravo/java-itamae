62642e3fa79330988a12aa059fd96288cb47883b
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-07 01:46:13 +0900

    modified: src/main/java/java_itamae/domain/model/template/IsValidSource.java
    modified message: {java_itamae_contents -> java_itamae}.domain.model.template.IsValidSource

--- src/main/java/java_itamae/domain/model/template/IsValidSource.java
+++ src/main/java/java_itamae/domain/model/template/IsValidSource.java
@@ -3,18 +3,19 @@ package java_itamae.domain.model.template;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 @Documented
 @Retention(RUNTIME)
 @Target(TYPE)
 @Constraint(validatedBy = {IsValidSourceValidator.class})
 public @interface IsValidSource {
-  String message() default "{java_itamae_contents.domain.model.template.IsValidSource}";
+  String message() default "{java_itamae.domain.model.template.IsValidSource}";
 
   Class<?>[] groups() default {};
 
