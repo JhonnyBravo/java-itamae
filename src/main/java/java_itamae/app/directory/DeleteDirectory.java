67dadc0987f7e3a6eecbc4b4ce08d71a0b526e86
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-06 18:20:38 +0900

    modified: src/main/java/java_itamae/app/directory/DeleteDirectory.java
    モデルクラスの変更に伴うコード修正。
    Attribute -> DirectoryResourceModel

--- src/main/java/java_itamae/app/directory/DeleteDirectory.java
+++ src/main/java/java_itamae/app/directory/DeleteDirectory.java
@@ -1,46 +1,46 @@
 package java_itamae.app.directory;
 
-import java.util.function.BiFunction;
-import java_itamae.domain.model.Attribute;
-import java_itamae.domain.service.directory.DirectoryService;
-import java_itamae.domain.service.directory.DirectoryServiceImpl;
+import java.util.function.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * ディレクトリを削除する。
- */
-public class DeleteDirectory implements BiFunction<Attribute, Boolean, Integer> {
+import java_itamae.domain.model.directory.DirectoryResourceModel;
+import java_itamae.domain.service.directory.DirectoryService;
+import java_itamae.domain.service.directory.DirectoryServiceImpl;
+
+/** ディレクトリを削除する。 */
+public class DeleteDirectory implements Function<DirectoryResourceModel, Integer> {
   /**
    * ディレクトリを削除する。
    *
-   * @param attr 操作対象とするディレクトリの情報を収めた Attribute を指定する。
+   * @param model 操作対象とするディレクトリの情報を収めた Attribute を指定する。
    * @param recursive
-   *        <ul>
-   *        <li>true: 操作対象とするディレクトリの配下に存在するファイル・ディレクトリも含めて削除する。</li>
-   *        <li>false: ディレクトリを削除する。操作対象とするディレクトリの配下にファイル・ディレクトリが存在する場合はエラーとなる。</li>
-   *        </ul>
+   *     <ul>
+   *       <li>true: 操作対象とするディレクトリの配下に存在するファイル・ディレクトリも含めて削除する。
+   *       <li>false: ディレクトリを削除する。操作対象とするディレクトリの配下にファイル・ディレクトリが存在する場合はエラーとなる。
+   *     </ul>
+   *
    * @return status
-   *         <ul>
-   *         <li>0: 操作を実行しなかったことを表す。</li>
-   *         <li>1: エラーが発生したことを表す。</li>
-   *         <li>2: 操作を実行したことを表す。</li>
-   *         </ul>
+   *     <ul>
+   *       <li>0: 操作を実行しなかったことを表す。
+   *       <li>1: エラーが発生したことを表す。
+   *       <li>2: 操作を実行したことを表す。
+   *     </ul>
    */
   @Override
-  public Integer apply(Attribute attr, Boolean recursive) {
+  public Integer apply(DirectoryResourceModel model) {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     final DirectoryService service = new DirectoryServiceImpl();
-    service.setRecursive(recursive);
+    service.setRecursive(model.isRecursive());
 
     try {
-      final boolean result = service.delete(attr);
+      final boolean result = service.delete(model);
 
       if (result) {
         return 2;
-      } else {
-        return 0;
       }
+      return 0;
     } catch (final Exception e) {
       if (logger.isWarnEnabled()) {
         logger.warn(e.toString());
@@ -49,5 +49,4 @@ public class DeleteDirectory implements BiFunction<Attribute, Boolean, Integer> 
       return 1;
     }
   }
-
 }
