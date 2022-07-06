2dd34c7aa00cc2a415caa45215a9822c1fd6ac02
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-06 18:11:41 +0900

    new file: src/main/java/java_itamae/domain/model/directory/DirectoryResourceModelValidator.java

---
+++ src/main/java/java_itamae/domain/model/directory/DirectoryResourceModelValidator.java
@@ -0,0 +1,44 @@
+package java_itamae.domain.model.directory;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+/** {@link DirectoryResourceModel} のバリデーションチェックを実行する。 */
+public class DirectoryResourceModelValidator implements Predicate<DirectoryResourceModel> {
+  /**
+   * {@link DirectoryResourceModel} のバリデーションチェックを実行する。
+   *
+   * @param model 判定対象とする {@link DirectoryResourceModel} を指定する。
+   * @return isValid
+   *     <ul>
+   *       <li>true: エラーが無いことを表す。
+   *       <li>false: エラーが発生したことを表す。
+   *     </ul>
+   */
+  @Override
+  public boolean test(DirectoryResourceModel model) {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+    final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    final Set<ConstraintViolation<DirectoryResourceModel>> resultSet = validator.validate(model);
+
+    if (resultSet.isEmpty()) {
+      return true;
+    }
+    resultSet.stream()
+        .forEach(
+            error -> {
+              final String path = error.getPropertyPath().toString();
+              final String message = error.getMessage();
+              logger.warn("{}: {}", path, message);
+            });
+
+    return false;
+  }
+}
