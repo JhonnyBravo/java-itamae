9260c2a8508b04f0db9f0a64e860a5dec7219bb0
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-07 01:42:56 +0900

    modified: src/main/java/java_itamae/domain/model/template/IsValidPath.java
    modified message: {java_itamae_contents -> java_itamae}.domain.model.template.IsValidPath

--- src/main/java/java_itamae/domain/model/template/IsValidPath.java
+++ src/main/java/java_itamae/domain/model/template/IsValidPath.java
@@ -3,18 +3,19 @@ package java_itamae.domain.model.template;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 @Documented
 @Retention(RUNTIME)
 @Target(TYPE)
 @Constraint(validatedBy = {IsValidPathValidator.class})
 public @interface IsValidPath {
-  String message() default "{java_itamae_contents.domain.model.template.IsValidPath}";
+  String message() default "{java_itamae.domain.model.template.IsValidPath}";
 
   Class<?>[] groups() default {};
 
