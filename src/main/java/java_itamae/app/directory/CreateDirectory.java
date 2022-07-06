f39e394f0baa475bfc1809ef94067c710072d3d4
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-06 18:19:45 +0900

    modified: src/main/java/java_itamae/app/directory/CreateDirectory.java
    モデルクラスの変更に伴うコード修正。
    Attribute -> DirectoryResourceModel

--- src/main/java/java_itamae/app/directory/CreateDirectory.java
+++ src/main/java/java_itamae/app/directory/CreateDirectory.java
@@ -1,58 +1,62 @@
 package java_itamae.app.directory;
 
-import java.util.function.BiFunction;
-import java_itamae.domain.model.Attribute;
-import java_itamae.domain.service.directory.DirectoryService;
-import java_itamae.domain.service.directory.DirectoryServiceImpl;
+import java.util.function.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java_itamae.domain.model.directory.DirectoryResourceModel;
+import java_itamae.domain.service.directory.DirectoryService;
+import java_itamae.domain.service.directory.DirectoryServiceImpl;
+
 /**
  * ディレクトリに対して以下の操作を実行する。
+ *
  * <ul>
- * <li>新規作成</li>
- * <li>所有者の変更</li>
- * <li>グループ所有者の変更</li>
- * <li>パーミッションの変更</li>
+ *   <li>新規作成
+ *   <li>所有者の変更
+ *   <li>グループ所有者の変更
+ *   <li>パーミッションの変更
  * </ul>
  */
-public class CreateDirectory implements BiFunction<Attribute, Boolean, Integer> {
+public class CreateDirectory implements Function<DirectoryResourceModel, Integer> {
   /**
    * ディレクトリに対して以下の操作を実行する。
+   *
    * <ul>
-   * <li>新規作成</li>
-   * <li>所有者の変更</li>
-   * <li>グループ所有者の変更</li>
-   * <li>パーミッションの変更</li>
+   *   <li>新規作成
+   *   <li>所有者の変更
+   *   <li>グループ所有者の変更
+   *   <li>パーミッションの変更
    * </ul>
    *
-   * @param attr 操作対象とするディレクトリの情報を収めた Attribute を指定する。
+   * @param model 操作対象とするディレクトリの情報を収めた Attribute を指定する。
    * @param recursive
-   *        <ul>
-   *        <li>true: 複数階層のディレクトリを親ディレクトリも含めて作成する。</li>
-   *        <li>false: 単一階層のディレクトリを作成する。</li>
-   *        </ul>
+   *     <ul>
+   *       <li>true: 複数階層のディレクトリを親ディレクトリも含めて作成する。
+   *       <li>false: 単一階層のディレクトリを作成する。
+   *     </ul>
+   *
    * @return status
-   *         <ul>
-   *         <li>0: 操作を実行しなかったことを表す。</li>
-   *         <li>1: エラーが発生したことを表す。</li>
-   *         <li>2: 操作を実行したことを表す。</li>
-   *         </ul>
+   *     <ul>
+   *       <li>0: 操作を実行しなかったことを表す。
+   *       <li>1: エラーが発生したことを表す。
+   *       <li>2: 操作を実行したことを表す。
+   *     </ul>
    */
   @Override
-  public Integer apply(Attribute attr, Boolean recursive) {
+  public Integer apply(DirectoryResourceModel model) {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     final DirectoryService service = new DirectoryServiceImpl();
-    service.setRecursive(recursive);
+    service.setRecursive(model.isRecursive());
 
     try {
-      final boolean result = service.create(attr);
+      final boolean result = service.create(model);
 
       if (result) {
         return 2;
-      } else {
-        return 0;
       }
+      return 0;
     } catch (final Exception e) {
       if (logger.isWarnEnabled()) {
         logger.warn(e.toString());
@@ -61,5 +65,4 @@ public class CreateDirectory implements BiFunction<Attribute, Boolean, Integer> 
       return 1;
     }
   }
-
 }
