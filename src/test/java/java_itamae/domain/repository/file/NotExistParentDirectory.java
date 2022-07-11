1d24178d7a517386081ad073863e8a7a47fdc184
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-12 00:52:11 +0900

    new file: src/test/java/java_itamae/domain/repository/file/NotExistParentDirectory.java

---
+++ src/test/java/java_itamae/domain/repository/file/NotExistParentDirectory.java
@@ -0,0 +1,34 @@
+package java_itamae.domain.repository.file;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+import java.nio.file.NoSuchFileException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/** 親ディレクトリが存在しない場合のテスト */
+public class NotExistParentDirectory {
+  private FileRepository fr;
+  private File file;
+
+  @Before
+  public void setUp() throws Exception {
+    fr = new FileRepositoryImpl();
+    file = new File("NotExist/test.txt");
+  }
+
+  /** {@link NoSuchFileException} が送出されること。 */
+  @Test(expected = NoSuchFileException.class)
+  public void fre001() throws Exception {
+    try {
+      fr.create("NotExist/test.txt");
+    } catch (final Exception e) {
+      System.err.println(e);
+      assertThat(file.isFile(), is(false));
+      throw e;
+    }
+  }
+}
