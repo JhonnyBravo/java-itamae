29dfc440f158bb1e58d0a350b8eaff6bb4a71bce
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 01:17:27 +0900

    renamed: src/test/java/java_itamae/domain/{repository/directory/DirectoryRepositoryTest.java -> component/directory/DirectoryComponentTest.java}

--- src/test/java/java_itamae/domain/component/directory/DirectoryComponentTest.java
+++ src/test/java/java_itamae/domain/component/directory/DirectoryComponentTest.java
@@ -1,10 +1,10 @@
-package java_itamae.domain.repository.directory;
+package java_itamae.domain.component.directory;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/** {@link DirectoryRepository} の単体テスト。 */
+/** {@link DirectoryComponent} の単体テスト。 */
 @RunWith(Suite.class)
 @SuiteClasses({EmptyDirectory.class, NotEmptyDirectory.class, NotExistDirectory.class})
-public class DirectoryRepositoryTest {}
+public class DirectoryComponentTest {}
