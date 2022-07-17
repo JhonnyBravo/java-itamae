393bcfb71c970490e7bfa218d6d0be3960b28b4f
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 00:52:17 +0900

    renamed: src/test/java/java_itamae/domain/{repository -> component}/file/ExistFile.java

--- src/test/java/java_itamae/domain/component/file/ExistFile.java
+++ src/test/java/java_itamae/domain/component/file/ExistFile.java
@@ -1,29 +1,32 @@
-package java_itamae.domain.repository.file;
+package java_itamae.domain.component.file;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /** ファイルが既に存在する場合のテスト */
 public class ExistFile {
-  private FileRepository fr;
-  private File file;
+  private FileComponent component;
+  private Path path;
 
   @Before
   public void setUp() throws Exception {
-    fr = new FileRepositoryImpl();
-    file = new File("test.txt");
-    file.createNewFile();
+    component = new FileComponentImpl();
+    path = component.convertToPath("test.txt");
+    Files.createFile(path);
   }
 
   @After
   public void tearDown() throws Exception {
-    if (file.isFile()) {
-      file.delete();
+    if (path.toFile().isFile()) {
+      Files.delete(path);
     }
   }
 
@@ -32,13 +35,13 @@ public class ExistFile {
    *
    * <ul>
    *   <li>何も実行されないこと。
-   *   <li>終了ステータスが false であること。
+   *   <li>終了ステータスが 0 であること。
    * </ul>
    */
   @Test
   public void frs001() throws Exception {
-    final boolean status = fr.create("test.txt");
-    assertThat(status, is(false));
+    final int status = component.create(path.toFile().getPath());
+    assertThat(status, is(0));
   }
 
   /**
@@ -46,13 +49,14 @@ public class ExistFile {
    *
    * <ul>
    *   <li>ファイルが削除されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void frs002() throws Exception {
-    final boolean status = fr.delete("test.txt");
-    assertThat(status, is(true));
+    final File file = path.toFile();
+    final int status = component.delete(file.getPath());
+    assertThat(status, is(2));
     assertThat(file.isFile(), is(false));
   }
 }
