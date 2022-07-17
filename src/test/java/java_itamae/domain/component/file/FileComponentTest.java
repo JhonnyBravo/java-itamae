e8a7d0fd284588e3e180dea82b76e679a0a8050e
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 00:59:45 +0900

    renamed: src/test/java/java_itamae/domain/{repository/file/FileRepositoryTest.java -> component/file/FileComponentTest.java}

--- src/test/java/java_itamae/domain/component/file/FileComponentTest.java
+++ src/test/java/java_itamae/domain/component/file/FileComponentTest.java
@@ -1,10 +1,10 @@
-package java_itamae.domain.repository.file;
+package java_itamae.domain.component.file;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/** {@link java_itamae.domain.repository.file.FileRepository} の単体テスト。 */
+/** {@link FileComponent} の単体テスト。 */
 @RunWith(Suite.class)
 @SuiteClasses({ExistFile.class, NotExistFile.class, NotExistParentDirectory.class})
-public class FileRepositoryTest {}
+public class FileComponentTest {}
