a6ba3525282e54ade70755e96fff0e9d3ea6bde4
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:27:11 +0900

    modified: src/main/java/java_itamae/domain/service/directory/DirectoryService.java
    modified methods: create, delete
      戻り値を boolean から int へ変更。

--- src/main/java/java_itamae/domain/service/directory/DirectoryService.java
+++ src/main/java/java_itamae/domain/service/directory/DirectoryService.java
@@ -1,55 +1,33 @@
 package java_itamae.domain.service.directory;
 
 import java_itamae.domain.model.directory.DirectoryResourceModel;
+import java_itamae.domain.service.common.BaseService;
 
 /** ディレクトリの操作を管理する。 */
-public interface DirectoryService {
-  /**
-   * ディレクトリを再帰的に操作するかどうかを設定する。
-   *
-   * @param recursive
-   *     <ul>
-   *       <li>true: ディレクトリを再帰的に作成 / 削除するように設定する。
-   *       <li>false: ディレクトリを再帰的に作成 / 削除しないように設定する。(既定値)
-   *     </ul>
-   */
-  void setRecursive(boolean recursive);
-
+public interface DirectoryService extends BaseService {
   /**
    * ディレクトリを作成し、所有者・グループ所有者・パーミッションの設定を変更する。
    *
-   * @param attr 操作対象とするディレクトリの設定情報を納めた Attribute オブジェクトを指定する。
+   * @param model 操作対象とするディレクトリの情報を納めた {@link DirectoryResourceModel} を指定する。
    * @return status
-   *     <p>以下の場合に true を返す。
    *     <ul>
-   *       <li>ディレクトリが新規作成された場合
-   *       <li>ディレクトリの所有者が変更された場合
-   *       <li>ディレクトリのグループ所有者が変更された場合
-   *       <li>ディレクトリのパーミッション設定が変更された場合
+   *       <li>0: 操作を実行せずに正常終了したことを表す。
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: 操作を実行して正常終了したことを表す。
    *     </ul>
-   *     <p>以下の場合に false を返す。
-   *     <ul>
-   *       <li>ディレクトリが作成されなかった場合
-   *       <li>ディレクトリの所有者が変更されなかった場合
-   *       <li>ディレクトリのグループ所有者が変更されなかった場合
-   *       <li>ディレクトリのパーミッション設定が変更されなかった場合
-   *     </ul>
-   *
-   * @throws Exception {@link java.lang.Exception}
    */
-  boolean create(DirectoryResourceModel attr) throws Exception;
+  int create(DirectoryResourceModel model);
 
   /**
    * ディレクトリを削除する。
    *
-   * @param attr 操作対象とするディレクトリの設定情報を納めた Attribute オブジェクトを指定する。
+   * @param model 操作対象とするディレクトリの情報を納めた {@link DirectoryResourceModel} を指定する。
    * @return status
    *     <ul>
-   *       <li>true: ディレクトリが削除されたことを表す。
-   *       <li>false: ディレクトリが削除されなかったことを表す。
+   *       <li>0: 操作を実行せずに正常終了したことを表す。
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: 操作を実行して正常終了したことを表す。
    *     </ul>
-   *
-   * @throws Exception {@link java.lang.Exception}
    */
-  boolean delete(DirectoryResourceModel attr) throws Exception;
+  int delete(DirectoryResourceModel model);
 }
