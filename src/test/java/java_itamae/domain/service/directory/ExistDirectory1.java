6321e14ea6040a6f06af1b80034c39e9fca7d79d
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:32:06 +0900

    modified: src/test/java/java_itamae/domain/service/directory/ExistDirectory1.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/directory/ExistDirectory1.java
+++ src/test/java/java_itamae/domain/service/directory/ExistDirectory1.java
@@ -3,94 +3,126 @@ package java_itamae.domain.service.directory;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java_itamae.domain.common.IsWindows;
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.model.directory.DirectoryResourceModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 /** ディレクトリが存在する場合のテスト。 */
 public class ExistDirectory1 {
   private IsWindows isWindows;
   private DirectoryService ds;
   private PropertiesService ps;
-  private File file;
+  private Path path;
 
   @Before
   public void setUp() throws Exception {
     isWindows = new IsWindows();
-    final ContentsModel ca = new ContentsModel();
-    ca.setPath("src/test/resources/test.properties");
-    ps = new PropertiesServiceImpl(ca);
+    final ContentsModel cm = new ContentsModel();
+    cm.setPath("src/test/resources/test.properties");
+    ps = new PropertiesServiceImpl(cm);
 
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setOwner(ps.getProperty("owner"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath("test_dir");
+    model.setOwner(ps.getProperty("owner"));
 
     if (!isWindows.get()) {
-      attr.setGroup(ps.getProperty("group"));
-      attr.setMode("640");
+      model.setGroup(ps.getProperty("group"));
+      model.setMode("640");
     }
 
     ds = new DirectoryServiceImpl();
-    ds.create(attr);
+    ds.create(model);
 
-    file = new File("test_dir");
+    path = FileSystems.getDefault().getPath(model.getPath());
   }
 
   @After
   public void tearDown() throws Exception {
-    if (file.isDirectory()) {
-      file.delete();
+    if (path.toFile().isDirectory()) {
+      Files.delete(path);
     }
   }
 
-  /** path に指定したディレクトリが既に存在する場合に終了ステータスが false であること。 */
+  /**
+   * path に指定したディレクトリが既に存在する場合に
+   *
+   * <ul>
+   *   <li>操作が実行されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   @Test
   public void dss001() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(false));
+    final int status = ds.create(model);
+    assertThat(status, is(0));
   }
 
-  /** 新しいディレクトリ所有者のユーザ名と、現在設定されているディレクトリ所有者のユーザ名が同一である場合に終了ステータスが false であること。 */
+  /**
+   * 新しいディレクトリ所有者のユーザ名と、現在設定されているディレクトリ所有者のユーザ名が同一である場合に
+   *
+   * <ul>
+   *   <li>所有者が変更されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   @Test
   public void dss002() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setOwner(ps.getProperty("owner"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner(ps.getProperty("owner"));
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(false));
+    final int status = ds.create(model);
+    assertThat(status, is(0));
   }
 
-  /** 新しいグループ所有者のグループ名と、現在設定されているグループ所有者のグループ名が同一である場合に終了ステータスが false であること。 */
+  /**
+   * 新しいグループ所有者のグループ名と、現在設定されているグループ所有者のグループ名が同一である場合に
+   *
+   * <ul>
+   *   <li>グループ所有者が変更されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
   @Test
   public void dss003() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setGroup(ps.getProperty("group"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setGroup(ps.getProperty("group"));
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(false));
+    final int status = ds.create(model);
+    assertThat(status, is(0));
   }
 
-  /** 新しいパーミッション設定値と、現在設定されているパーミッション設定値が同一である場合に終了ステータスが false であること。 */
+  /**
+   * 新しいパーミッション設定値と、現在設定されているパーミッション設定値が同一である場合に
+   *
+   * <ul>
+   *   <li>パーミッション設定値が変更されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
   @Test
   public void dss004() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setMode("640");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setMode("640");
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(false));
+    final int status = ds.create(model);
+    assertThat(status, is(0));
   }
 }
