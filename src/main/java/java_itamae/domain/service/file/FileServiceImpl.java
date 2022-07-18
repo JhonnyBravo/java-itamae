10ba13070be075546de6ebe1627e430fd328b35e
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:19:19 +0900

    modified: src/main/java/java_itamae/domain/service/file/FileServiceImpl.java
    modified methods: create, delete
      Repository クラスの名称変更、ならびに仕様変更に伴うコード修正。

--- src/main/java/java_itamae/domain/service/file/FileServiceImpl.java
+++ src/main/java/java_itamae/domain/service/file/FileServiceImpl.java
@@ -1,49 +1,81 @@
 package java_itamae.domain.service.file;
 
+import java_itamae.domain.component.file.FileComponent;
+import java_itamae.domain.component.file.FileComponentImpl;
+import java_itamae.domain.component.group.GroupComponent;
+import java_itamae.domain.component.group.GroupComponentImpl;
+import java_itamae.domain.component.mode.ModeComponent;
+import java_itamae.domain.component.mode.ModeComponentImpl;
+import java_itamae.domain.component.owner.OwnerComponent;
+import java_itamae.domain.component.owner.OwnerComponentImpl;
 import java_itamae.domain.model.file.FileResourceModel;
-import java_itamae.domain.repository.file.FileRepository;
-import java_itamae.domain.repository.file.FileRepositoryImpl;
-import java_itamae.domain.repository.group.GroupRepository;
-import java_itamae.domain.repository.group.GroupRepositoryImpl;
-import java_itamae.domain.repository.mode.ModeRepository;
-import java_itamae.domain.repository.mode.ModeRepositoryImpl;
-import java_itamae.domain.repository.owner.OwnerRepository;
-import java_itamae.domain.repository.owner.OwnerRepositoryImpl;
 
 public class FileServiceImpl implements FileService {
-  private final FileRepository fr;
-  private final OwnerRepository or;
-  private final GroupRepository gr;
-  private final ModeRepository mr;
+  private final FileComponent fc;
+  private final OwnerComponent oc;
+  private final GroupComponent gc;
+  private final ModeComponent mc;
 
   public FileServiceImpl() {
-    fr = new FileRepositoryImpl();
-    or = new OwnerRepositoryImpl();
-    gr = new GroupRepositoryImpl();
-    mr = new ModeRepositoryImpl();
+    fc = new FileComponentImpl();
+    oc = new OwnerComponentImpl();
+    gc = new GroupComponentImpl();
+    mc = new ModeComponentImpl();
   }
 
   @Override
-  public boolean create(FileResourceModel attr) throws Exception {
-    boolean status = fr.create(attr.getPath());
+  public int create(FileResourceModel model) {
+    int status = 0;
 
-    if (attr.getOwner() != null) {
-      status = or.updateOwner(attr.getPath(), attr.getOwner());
-    }
+    try {
+      status = fc.create(model.getPath());
 
-    if (attr.getGroup() != null) {
-      status = gr.updateGroup(attr.getPath(), attr.getGroup());
-    }
+      if (status == 1) {
+        return status;
+      }
 
-    if (attr.getMode() != null) {
-      status = mr.updateMode(attr.getPath(), attr.getMode());
+      if (model.getOwner() != null) {
+        status = oc.updateOwner(model.getPath(), model.getOwner());
+
+        if (status == 1) {
+          return status;
+        }
+      }
+
+      if (model.getGroup() != null) {
+        status = gc.updateGroup(model.getPath(), model.getGroup());
+
+        if (status == 1) {
+          return status;
+        }
+      }
+
+      if (model.getMode() != null) {
+        status = mc.updateMode(model.getPath(), model.getMode());
+
+        if (status == 1) {
+          return status;
+        }
+      }
+    } catch (final Exception e) {
+      this.getLogger().warn(e.toString());
+      status = 1;
     }
 
     return status;
   }
 
   @Override
-  public boolean delete(FileResourceModel attr) throws Exception {
-    return fr.delete(attr.getPath());
+  public int delete(FileResourceModel model) {
+    int status = 0;
+
+    try {
+      status = fc.delete(model.getPath());
+    } catch (final Exception e) {
+      this.getLogger().warn(e.toString());
+      status = 1;
+    }
+
+    return status;
   }
 }
