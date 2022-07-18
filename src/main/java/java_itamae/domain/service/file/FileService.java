792e64fdfb27664a3bb1242b9f194bdf76128607
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:17:49 +0900

    modified: src/main/java/java_itamae/domain/service/file/FileService.java
    modified methods: create, delete
      戻り値を boolean から int へ変更。

--- src/main/java/java_itamae/domain/service/file/FileService.java
+++ src/main/java/java_itamae/domain/service/file/FileService.java
@@ -1,44 +1,33 @@
 package java_itamae.domain.service.file;
 
 import java_itamae.domain.model.file.FileResourceModel;
+import java_itamae.domain.service.common.BaseService;
 
 /** ファイルの操作を管理する。 */
-public interface FileService {
+public interface FileService extends BaseService {
   /**
    * ファイルを作成し、所有者・グループ所有者・パーミッションの設定を変更する。
    *
-   * @param attr 操作対象とするファイルの設定情報を納めた Attribute オブジェクトを指定する。
+   * @param model 操作対象とするファイルの情報を納めた {@link FileResourceModel} を指定する。
    * @return status
-   *     <p>以下の場合に true を返す。
    *     <ul>
-   *       <li>ファイルが新規作成された場合
-   *       <li>ファイルの所有者が変更された場合
-   *       <li>ファイルのグループ所有者が変更された場合
-   *       <li>ファイルのパーミッション設定が変更された場合
+   *       <li>0: 操作を実行せずに正常終了したことを表す。
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: 操作を実行して正常終了したことを表す。
    *     </ul>
-   *     <p>以下の場合に false を返す。
-   *     <ul>
-   *       <li>ファイルが作成されなかった場合
-   *       <li>ファイルの所有者が変更されなかった場合
-   *       <li>ファイルのグループ所有者が変更されなかった場合
-   *       <li>ファイルのパーミッション設定が変更されなかった場合
-   *     </ul>
-   *
-   * @throws Exception {@link java.lang.Exception}
    */
-  boolean create(FileResourceModel attr) throws Exception;
+  int create(FileResourceModel model);
 
   /**
    * ファイルを削除する。
    *
-   * @param attr 操作対象とするファイルの設定情報を納めた Attribute オブジェクトを指定する。
+   * @param model 操作対象とするファイルの情報を納めた {@link FileResourceModel} を指定する。
    * @return status
    *     <ul>
-   *       <li>true: ファイルが削除されたことを表す。
-   *       <li>false: ファイルが削除されなかったことを表す。
+   *       <li>0: 操作を実行せずに正常終了したことを表す。
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: 操作を実行して正常終了したことを表す。
    *     </ul>
-   *
-   * @throws Exception {@link java.lang.Exception}
    */
-  boolean delete(FileResourceModel attr) throws Exception;
+  int delete(FileResourceModel model);
 }
