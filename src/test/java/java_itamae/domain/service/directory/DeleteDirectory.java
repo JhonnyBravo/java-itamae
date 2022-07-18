cef59728e5306eaf90d05394f10ca2e08c45c478
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:30:09 +0900

    modified: src/test/java/java_itamae/domain/service/directory/DeleteDirectory.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/directory/DeleteDirectory.java
+++ src/test/java/java_itamae/domain/service/directory/DeleteDirectory.java
@@ -4,9 +4,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
-import java_itamae.domain.model.directory.DirectoryResourceModel;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import java_itamae.domain.model.directory.DirectoryResourceModel;
 
 /** ディレクトリを削除する場合のテスト。 */
 public class DeleteDirectory {
@@ -19,39 +24,48 @@ public class DeleteDirectory {
 
   /**
    * 削除対象とするディレクトリが存在しない状態で recursive を指定せずに {@link DirectoryService#delete(DirectoryResourceModel)}
-   * を実行した場合に終了ステータスが false であること。
+   * を実行した場合に
+   *
+   * <ul>
+   *   <li>操作が実行されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
    */
   @Test
   public void dss001() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath("test_dir");
 
-    final boolean status = ds.delete(attr);
-    assertThat(status, is(false));
+    final int status = ds.delete(model);
+    assertThat(status, is(0));
   }
 
   /**
    * 削除対象とするディレクトリが存在しない状態で recursive に true を設定して {@link
-   * DirectoryService#delete(DirectoryResourceModel)} を実行した場合に終了ステータスが false であること。
+   * DirectoryService#delete(DirectoryResourceModel)} を実行した場合に
+   *
+   * <ul>
+   *   <li>操作が実行されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
    */
   @Test
   public void dss002() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath("test_dir");
+    model.setRecursive("true");
 
-    ds.setRecursive(true);
-    final boolean status = ds.delete(attr);
-
-    assertThat(status, is(false));
+    final int status = ds.delete(model);
+    assertThat(status, is(0));
   }
 
   /**
-   *
+   * 削除対象とするディレクトリが存在する状態で recursive を指定せずに {@link DirectoryService#delete(DirectoryResourceModel)}
+   * を実行した場合に
    *
    * <ul>
-   *   <li>削除対象とするディレクトリが存在する状態で recursive を指定せずに {@link
-   *       DirectoryService#delete(DirectoryResourceModel)} を実行した場合にディレクトリが削除されること。
-   *   <li>終了ステータスが false であること。
+   *   <li>ディレクトリが削除されること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
@@ -59,36 +73,39 @@ public class DeleteDirectory {
     final File directory = new File("test_dir");
     directory.mkdir();
 
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath("test_dir");
 
-    final boolean status = ds.delete(attr);
-    assertThat(status, is(true));
+    final int status = ds.delete(model);
+    assertThat(status, is(2));
     assertThat(directory.isDirectory(), is(false));
   }
 
   /**
-   *
+   * 削除対象とするディレクトリが存在する状態で recursive に true を設定して {@link
+   * DirectoryService#delete(DirectoryResourceModel)} を実行した場合に
    *
    * <ul>
-   *   <li>削除対象とするディレクトリが存在する状態で recursive に true を設定して {@link
-   *       DirectoryService#delete(DirectoryResourceModel)} を実行した場合にディレクトリが一括削除されること。
-   *   <li>終了ステータスが false であること。
+   *   <li>ディレクトリが一括削除されること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void dss004() throws Exception {
-    final File directory = new File("parent/sub1/sub2");
-    directory.mkdirs();
+    // parent/sub1/sub2
+    final Path directory = FileSystems.getDefault().getPath("parent/sub1/sub2");
+    Files.createDirectories(directory);
 
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("parent");
+    // parent
+    final Path rootDir = directory.getParent().getParent();
 
-    ds.setRecursive(true);
-    final boolean status = ds.delete(attr);
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(rootDir.toFile().getPath());
+    model.setRecursive("true");
 
-    assertThat(status, is(true));
-    final File parent = new File("parent");
-    assertThat(parent.isDirectory(), is(false));
+    final int status = ds.delete(model);
+
+    assertThat(status, is(2));
+    assertThat(rootDir.toFile().isDirectory(), is(false));
   }
 }
