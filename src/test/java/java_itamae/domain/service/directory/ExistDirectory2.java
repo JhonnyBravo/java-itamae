9f7769c3dfd52c40b11b2c8df036d087694ff5ee
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:32:46 +0900

    modified: src/test/java/java_itamae/domain/service/directory/ExistDirectory2.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/directory/ExistDirectory2.java
+++ src/test/java/java_itamae/domain/service/directory/ExistDirectory2.java
@@ -3,37 +3,40 @@ package java_itamae.domain.service.directory;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.model.directory.DirectoryResourceModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 /** ディレクトリが存在する場合のテスト。 */
 public class ExistDirectory2 {
   private DirectoryService ds;
   private PropertiesService ps;
-  private File directory;
+  private Path path;
 
   @Before
   public void setUp() throws Exception {
-    directory = new File("test_dir");
-    directory.mkdir();
+    path = FileSystems.getDefault().getPath("test_dir");
+    Files.createDirectory(path);
 
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("src/test/resources/test.properties");
-    ps = new PropertiesServiceImpl(attr);
-
+    final ContentsModel model = new ContentsModel();
+    model.setPath("src/test/resources/test.properties");
+    ps = new PropertiesServiceImpl(model);
     ds = new DirectoryServiceImpl();
   }
 
   @After
   public void tearDown() throws Exception {
-    if (directory.isDirectory()) {
-      directory.delete();
+    if (path.toFile().isDirectory()) {
+      Files.delete(path);
     }
   }
 
@@ -42,17 +45,17 @@ public class ExistDirectory2 {
    *
    * <ul>
    *   <li>ディレクトリ所有者が変更されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void dss001() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setOwner(ps.getProperty("owner"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner(ps.getProperty("owner"));
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
   }
 
   /**
@@ -60,18 +63,18 @@ public class ExistDirectory2 {
    *
    * <ul>
    *   <li>グループ所有者が変更されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
   @Test
   public void dss002() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setGroup(ps.getProperty("group"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setGroup(ps.getProperty("group"));
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
   }
 
   /**
@@ -79,17 +82,17 @@ public class ExistDirectory2 {
    *
    * <ul>
    *   <li>パーミッション設定値が変更されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
   @Test
   public void dss003() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setMode("741");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setMode("741");
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
   }
 }
