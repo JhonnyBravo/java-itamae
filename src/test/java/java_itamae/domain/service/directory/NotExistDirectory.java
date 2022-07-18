057599690990f0ca31e23697b8f045f4d73a1e95
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:33:39 +0900

    modified: src/test/java/java_itamae/domain/service/directory/NotExistDirectory.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/directory/NotExistDirectory.java
+++ src/test/java/java_itamae/domain/service/directory/NotExistDirectory.java
@@ -3,35 +3,39 @@ package java_itamae.domain.service.directory;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.model.directory.DirectoryResourceModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 /** ディレクトリが存在しない場合のテスト。 */
 public class NotExistDirectory {
   private DirectoryService ds;
   private PropertiesService ps;
-  private File directory;
+  private Path path;
 
   @Before
   public void setUp() throws Exception {
     ds = new DirectoryServiceImpl();
-    directory = new File("test_dir");
+    path = FileSystems.getDefault().getPath("test_dir");
 
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("src/test/resources/test.properties");
-    ps = new PropertiesServiceImpl(attr);
+    final ContentsModel model = new ContentsModel();
+    model.setPath("src/test/resources/test.properties");
+    ps = new PropertiesServiceImpl(model);
   }
 
   @After
   public void tearDown() throws Exception {
-    if (directory.isDirectory()) {
-      directory.delete();
+    if (path.toFile().isDirectory()) {
+      Files.delete(path);
     }
   }
 
@@ -40,17 +44,17 @@ public class NotExistDirectory {
    *
    * <ul>
    *   <li>ディレクトリが作成されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void dss001() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
-    assertThat(directory.isDirectory(), is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
+    assertThat(path.toFile().isDirectory(), is(true));
   }
 
   /**
@@ -59,17 +63,18 @@ public class NotExistDirectory {
    * <ul>
    *   <li>ディレクトリが作成されること。
    *   <li>所有者が変更されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void dss002() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setOwner(ps.getProperty("owner"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner(ps.getProperty("owner"));
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
+    assertThat(path.toFile().isDirectory(), is(true));
   }
 
   /**
@@ -78,18 +83,19 @@ public class NotExistDirectory {
    * <ul>
    *   <li>ディレクトリが作成されること。
    *   <li>グループ所有者が変更されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
   @Test
   public void dss003() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setGroup(ps.getProperty("group"));
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setGroup(ps.getProperty("group"));
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
+    assertThat(path.toFile().isDirectory(), is(true));
   }
 
   /**
@@ -98,17 +104,18 @@ public class NotExistDirectory {
    * <ul>
    *   <li>ディレクトリが作成されること。
    *   <li>パーミッション設定値が変更されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
   @Test
   public void dss004() throws Exception {
-    final DirectoryResourceModel attr = new DirectoryResourceModel();
-    attr.setPath("test_dir");
-    attr.setMode("741");
+    final DirectoryResourceModel model = new DirectoryResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setMode("741");
 
-    final boolean status = ds.create(attr);
-    assertThat(status, is(true));
+    final int status = ds.create(model);
+    assertThat(status, is(2));
+    assertThat(path.toFile().isDirectory(), is(true));
   }
 }
