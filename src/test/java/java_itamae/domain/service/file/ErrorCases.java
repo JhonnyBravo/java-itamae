28a1724dd66e1a64a10435c38484abdb82243cdc
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:21:22 +0900

    modified: src/test/java/java_itamae/domain/service/file/ErrorCases.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/file/ErrorCases.java
+++ src/test/java/java_itamae/domain/service/file/ErrorCases.java
@@ -1,141 +1,157 @@
 package java_itamae.domain.service.file;
 
-import java.io.File;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.model.file.FileResourceModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 /** エラーケースのテスト */
 public class ErrorCases {
   private FileService fs;
   private PropertiesService ps;
-  private File file;
+  private Path path;
 
   @Before
   public void setUp() throws Exception {
     fs = new FileServiceImpl();
-    file = new File("test.txt");
+    path = FileSystems.getDefault().getPath("test.txt");
 
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("src/test/resources/test.properties");
-    ps = new PropertiesServiceImpl(attr);
+    final ContentsModel model = new ContentsModel();
+    model.setPath("src/test/resources/test.properties");
+    ps = new PropertiesServiceImpl(model);
   }
 
   @After
   public void tearDown() throws Exception {
-    if (file.isFile()) {
-      file.delete();
+    if (path.toFile().isFile()) {
+      Files.delete(path);
     }
   }
 
   /**
-   * path が指定されないまま {@link FileService#create(FileResourceModel)} を実行した場合に {@link
-   * NullPointerException} が送出されること。
+   * path が指定されないまま {@link FileService#create(FileResourceModel)} を実行した場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
    */
-  @Test(expected = NullPointerException.class)
+  @Test
   public void fse001() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setOwner(ps.getProperty("owner"));
-    attr.setGroup(ps.getProperty("group"));
-    attr.setMode("640");
+    final FileResourceModel model = new FileResourceModel();
+    model.setOwner(ps.getProperty("owner"));
+    model.setGroup(ps.getProperty("group"));
+    model.setMode("640");
 
-    try {
-      fs.create(attr);
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final int status = fs.create(model);
+    assertThat(status, is(1));
   }
 
   /**
-   * path が指定されないまま {@link FileService#delete(FileResourceModel)} を実行した場合に {@link
-   * NullPointerException} が送出されること。
+   * path が指定されないまま {@link FileService#delete(FileResourceModel)} を実行した場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
    */
-  @Test(expected = NullPointerException.class)
+  @Test
   public void fse002() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-
-    try {
-      fs.delete(attr);
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final FileResourceModel model = new FileResourceModel();
+    final int status = fs.delete(model);
+    assertThat(status, is(1));
   }
 
-  /** 親ディレクトリが存在しない場合に {@link NoSuchFileException} が送出されること。 */
-  @Test(expected = NoSuchFileException.class)
+  /**
+   * 親ディレクトリが存在しない場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
+  @Test
   public void fse003() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("NotExist/test.txt");
-    attr.setOwner(ps.getProperty("owner"));
-    attr.setGroup(ps.getProperty("group"));
-    attr.setMode("640");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath("NotExist/test.txt");
+    model.setOwner(ps.getProperty("owner"));
+    model.setGroup(ps.getProperty("group"));
+    model.setMode("640");
 
-    try {
-      fs.create(attr);
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final int status = fs.create(model);
+    assertThat(status, is(1));
   }
 
-  /** 新しいファイル所有者として存在しないユーザ名を指定した場合に {@link UserPrincipalNotFoundException} が送出されること。 */
-  @Test(expected = UserPrincipalNotFoundException.class)
+  /**
+   * 新しいファイル所有者として、存在しないユーザの名前を指定した場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
+  @Test
   public void fse004() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setOwner("NotExist");
-    attr.setGroup(ps.getProperty("group"));
-    attr.setMode("640");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner("NotExist");
+    model.setGroup(ps.getProperty("group"));
+    model.setMode("640");
 
-    try {
-      fs.create(attr);
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final int status = fs.create(model);
+    assertThat(status, is(1));
   }
 
-  /** 新しいグループ所有者として存在しないグループ名を指定した場合に {@link UserPrincipalNotFoundException} が送出されること。 */
+  /**
+   * 新しいグループ所有者として、存在しないグループの名前を指定した場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
-  @Test(expected = UserPrincipalNotFoundException.class)
+  @Test
   public void fse005() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setOwner(ps.getProperty("owner"));
-    attr.setGroup("NotExist");
-    attr.setMode("640");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner(ps.getProperty("owner"));
+    model.setGroup("NotExist");
+    model.setMode("640");
 
-    try {
-      fs.create(attr);
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final int status = fs.create(model);
+    assertThat(status, is(1));
   }
 
-  /** 新しいパーミッション設定として不正なパーミッション設定値を指定した場合に {@link Exception} が送出されること。 */
+  /**
+   * 新しいパーミッション設定として不正なパーミッション設定値を指定した場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
   // @Ignore("Windows の場合は非対応である為、実行しない。")
-  @Test(expected = Exception.class)
+  @Test
   public void fse006() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setOwner(ps.getProperty("owner"));
-    attr.setGroup(ps.getProperty("group"));
-    attr.setMode("a40");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner(ps.getProperty("owner"));
+    model.setGroup(ps.getProperty("group"));
+    model.setMode("a40");
 
-    try {
-      fs.create(attr);
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final int status = fs.create(model);
+    assertThat(status, is(1));
   }
 }
