98d70ad6db8c639c744a8b58e96fab0d86405d2d
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-18 18:22:28 +0900

    modified: src/test/java/java_itamae/domain/service/file/ExistFile1.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/file/ExistFile1.java
+++ src/test/java/java_itamae/domain/service/file/ExistFile1.java
@@ -3,102 +3,127 @@ package java_itamae.domain.service.file;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java_itamae.domain.common.IsWindows;
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.model.file.FileResourceModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 /** ファイルが既に存在する場合のテスト */
 public class ExistFile1 {
   private FileService fs;
   private PropertiesService ps;
   private IsWindows isWindows;
-  private File file;
+  private Path path;
 
   @Before
   public void setUp() throws Exception {
     isWindows = new IsWindows();
-    final ContentsModel ca = new ContentsModel();
-    ca.setPath("src/test/resources/test.properties");
-    ps = new PropertiesServiceImpl(ca);
+    path = FileSystems.getDefault().getPath("test.txt");
 
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setOwner(ps.getProperty("owner"));
+    final ContentsModel cm = new ContentsModel();
+    cm.setPath("src/test/resources/test.properties");
+    ps = new PropertiesServiceImpl(cm);
+
+    final FileResourceModel frm = new FileResourceModel();
+    frm.setPath(path.toFile().getPath());
+    frm.setOwner(ps.getProperty("owner"));
 
     if (!isWindows.get()) {
-      attr.setGroup(ps.getProperty("group"));
-      attr.setMode("640");
+      frm.setGroup(ps.getProperty("group"));
+      frm.setMode("640");
     }
 
     fs = new FileServiceImpl();
-    fs.create(attr);
-
-    file = new File("test.txt");
+    fs.create(frm);
   }
 
   @After
   public void tearDown() throws Exception {
-    if (file.isFile()) {
-      file.delete();
+    if (path.toFile().isFile()) {
+      Files.delete(path);
     }
   }
 
   /**
-   *
+   * 操作対象とするファイルが既に存在する場合に
    *
    * <ul>
-   *   <li>何も実行されないこと。
-   *   <li>終了ステータスが false であること。
+   *   <li>操作が実行されないこと。
+   *   <li>終了ステータスが 0 であること。
    * </ul>
    */
   @Test
   public void fss001() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
 
-    final boolean status = fs.create(attr);
-    assertThat(status, is(false));
+    final int status = fs.create(model);
+    assertThat(status, is(0));
   }
 
-  /** 新しいファイル所有者のユーザ名と、現在ファイルに設定されている所有者のユーザ名が同一である場合に、終了ステータスが false であること。 */
+  /**
+   * 新しいファイル所有者のユーザ名と、現在ファイルに設定されている所有者のユーザ名が同一である場合に
+   *
+   * <ul>
+   *   <li>ファイル所有者が変更されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   @Test
   public void fss002() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setOwner(ps.getProperty("owner"));
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setOwner(ps.getProperty("owner"));
 
-    final boolean status = fs.create(attr);
-    assertThat(status, is(false));
+    final int status = fs.create(model);
+    assertThat(status, is(0));
   }
 
-  /** 新しいグループ所有者のグループ名と、現在ファイルに設定されているグループ所有者のグループ名が同一である場合に、終了ステータスが false であること。 */
+  /**
+   * 新しいグループ所有者のグループ名と、現在ファイルに設定されているグループ所有者のグループ名が同一である場合に
+   *
+   * <ul>
+   *   <li>グループ所有者が変更されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   // @Ignore("Windows の場合は非対応である為、実行しない")
   @Test
   public void fss003() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setGroup(ps.getProperty("group"));
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setGroup(ps.getProperty("group"));
 
-    final boolean status = fs.create(attr);
-    assertThat(status, is(false));
+    final int status = fs.create(model);
+    assertThat(status, is(0));
   }
 
-  /** 新しいパーミッション設定値と、現在ファイルに設定されているパーミッション設定値が同一である場合に、終了ステータスが false であること。 */
+  /**
+   * 新しいパーミッション設定値と、現在ファイルに設定されているパーミッション設定値が同一である場合に
+   *
+   * <ul>
+   *   <li>パーミッション設定値が変更されないこと。
+   *   <li>終了ステータスが 0 であること。
+   * </ul>
+   */
   // @Ignore("Windows の場合は非対応である為、実行しない")
   @Test
   public void fss004() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
-    attr.setMode("640");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
+    model.setMode("640");
 
-    final boolean status = fs.create(attr);
-    assertThat(status, is(false));
+    final int status = fs.create(model);
+    assertThat(status, is(0));
   }
 
   /**
@@ -106,15 +131,16 @@ public class ExistFile1 {
    *
    * <ul>
    *   <li>{@link FileService#delete(FileResourceModel)} 実行時にファイルが削除されること。
-   *   <li>終了ステータスが true であること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void fss005() throws Exception {
-    final FileResourceModel attr = new FileResourceModel();
-    attr.setPath("test.txt");
+    final FileResourceModel model = new FileResourceModel();
+    model.setPath(path.toFile().getPath());
 
-    final boolean status = fs.delete(attr);
-    assertThat(status, is(true));
+    final int status = fs.delete(model);
+    assertThat(status, is(2));
+    assertThat(path.toFile().isFile(), is(false));
   }
 }
