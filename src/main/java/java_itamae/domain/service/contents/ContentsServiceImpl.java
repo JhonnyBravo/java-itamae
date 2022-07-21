476ff7522e31e788880d775ae9249112f5d10777
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-22 01:15:20 +0900

    modified: src/main/java/java_itamae/domain/service/contents/ContentsServiceImpl.java
    new method: init
    deleted method: updateContent, appendContent

--- src/main/java/java_itamae/domain/service/contents/ContentsServiceImpl.java
+++ src/main/java/java_itamae/domain/service/contents/ContentsServiceImpl.java
@@ -1,99 +1,38 @@
 package java_itamae.domain.service.contents;
 
-import java.io.File;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.ArrayList;
 import java.util.List;
+
+import java_itamae.domain.component.contents.ContentsComponent;
+import java_itamae.domain.component.contents.ContentsComponentImpl;
 import java_itamae.domain.model.contents.ContentsModel;
-import java_itamae.domain.repository.contents.ContentsRepository;
-import java_itamae.domain.repository.contents.ContentsRepositoryImpl;
-import java_itamae.domain.repository.stream.StreamRepository;
-import java_itamae.domain.repository.stream.StreamRepositoryImpl;
 
 public class ContentsServiceImpl implements ContentsService {
-  private final ContentsModel attr;
-  private final StreamRepository sr;
-  private final ContentsRepository cr;
+  private ContentsModel model;
+  private final ContentsComponent component;
 
-  public ContentsServiceImpl(ContentsModel attr) {
-    this.attr = attr;
-    sr = new StreamRepositoryImpl();
-    cr = new ContentsRepositoryImpl();
+  public ContentsServiceImpl() {
+    component = new ContentsComponentImpl();
+  }
+
+  @Override
+  public void init(ContentsModel model) {
+    this.model = model;
   }
 
   @Override
   public List<String> getContents() throws Exception {
-    List<String> contents = new ArrayList<>();
-
-    try (Reader reader = sr.getReader(attr)) {
-      contents = cr.getContents(reader);
-    }
-
-    return contents;
+    return component.getContents(this.model);
   }
 
   @Override
-  public boolean updateContents(List<String> contents) throws Exception {
-    boolean status = false;
-
-    try (Writer writer = sr.getWriter(attr)) {
-      status = cr.updateContents(writer, contents);
-    }
-
+  public int updateContents(List<String> contents) {
+    final int status = component.updateContents(this.model, contents);
     return status;
   }
 
   @Override
-  public boolean updateContent(String content) throws Exception {
-    boolean status = false;
-    final File file = new File(attr.getPath());
-
-    if (!file.isFile()) {
-      file.createNewFile();
-    }
-
-    final List<String> contents = new ArrayList<>();
-    contents.add(content);
-
-    try (Writer writer = sr.getWriter(attr)) {
-      status = cr.updateContents(writer, contents);
-    }
-
-    return status;
-  }
-
-  @Override
-  public boolean appendContent(String content) throws Exception {
-    boolean status = false;
-    final File file = new File(attr.getPath());
-
-    if (!file.isFile()) {
-      file.createNewFile();
-    }
-
-    final List<String> contents = getContents();
-    contents.add(content);
-
-    try (Writer writer = sr.getWriter(attr)) {
-      status = cr.updateContents(writer, contents);
-    }
-
-    return status;
-  }
-
-  @Override
-  public boolean deleteContents() throws Exception {
-    boolean status = false;
-
-    final List<String> contents = getContents();
-
-    if (contents.size() > 0) {
-      try (Writer writer = sr.getWriter(attr)) {
-        status = cr.deleteContents(writer);
-      }
-    }
-
+  public int deleteContents() {
+    final int status = component.deleteContents(this.model);
     return status;
   }
 }
