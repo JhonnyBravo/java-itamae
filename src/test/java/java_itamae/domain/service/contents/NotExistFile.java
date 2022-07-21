c490c89875ee6c27de0e82782ddc6431d8e5d630
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-22 01:20:00 +0900

    modified: src/test/java/java_itamae/domain/service/contents/NotExistFile.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/contents/NotExistFile.java
+++ src/test/java/java_itamae/domain/service/contents/NotExistFile.java
@@ -5,96 +5,69 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
-import java_itamae.domain.model.contents.ContentsModel;
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import java_itamae.domain.common.GetTestContents;
+import java_itamae.domain.model.contents.ContentsModel;
 
 /** ファイルが存在しない場合のテスト */
 public class NotExistFile {
   private File file;
-  private ContentsService cs;
+  private GetTestContents getTestContents;
+  private ContentsService service;
 
   @Before
   public void setUp() throws Exception {
-    file = new File("cs_test.txt");
+    getTestContents = new GetTestContents();
+    file = new File("NotExist.txt");
 
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("cs_test.txt");
-    cs = new ContentsServiceImpl(attr);
-  }
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-  @After
-  public void tearDown() throws Exception {
-    if (file.isFile()) {
-      file.delete();
-    }
+    service = new ContentsServiceImpl();
+    service.init(model);
   }
 
   /** {@link ContentsService#getContents()} 実行時に {@link FileNotFoundException} が送出されること。 */
   @Test(expected = FileNotFoundException.class)
   public void cse001() throws Exception {
     try {
-      cs.getContents();
+      service.getContents();
     } catch (final Exception e) {
       System.err.println(e);
       throw e;
     }
   }
 
-  /** {@link ContentsService#deleteContents()} 実行時に {@link FileNotFoundException} が送出されること。 */
-  @Test(expected = FileNotFoundException.class)
+  /**
+   * {@link ContentsService#deleteContents()} 実行時に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
+  @Test
   public void cse002() throws Exception {
-    try {
-      cs.deleteContents();
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
+    final int status = service.deleteContents();
+    assertThat(status, is(1));
+    assertThat(file.isFile(), is(false));
   }
 
   /**
-   *
+   * {@link ContentsService#updateContents(List)} 実行時に
    *
    * <ul>
-   *   <li>{@link ContentsService#updateContent(String)} 実行時にファイルが作成されること。
-   *   <li>ファイルへ文字列が書き込まれること。
-   *   <li>終了ステータスが true であること。
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
    * </ul>
    */
   @Test
-  public void css001() throws Exception {
-    final boolean status = cs.updateContent("書込みテスト");
-    assertThat(status, is(true));
-    assertThat(file.isFile(), is(true));
-
-    final List<String> contents = cs.getContents();
-    assertThat(contents.size(), is(1));
-    assertThat(contents.get(0), is("書込みテスト"));
-
-    System.out.println(contents.get(0));
-  }
-
-  /**
-   *
-   *
-   * <ul>
-   *   <li>{@link ContentsService#appendContent(String)} 実行時にファイルが作成されること。
-   *   <li>ファイルへ文字列が書き込まれること。
-   *   <li>終了ステータスが true であること。
-   * </ul>
-   */
-  @Test
-  public void css002() throws Exception {
-    final boolean status = cs.appendContent("追記テスト");
-    assertThat(status, is(true));
-    assertThat(file.isFile(), is(true));
-
-    final List<String> contents = cs.getContents();
-    assertThat(contents.size(), is(1));
-    assertThat(contents.get(0), is("追記テスト"));
-
-    System.out.println(contents.get(0));
+  public void cse003() throws Exception {
+    final int status = service.updateContents(getTestContents.get());
+    assertThat(status, is(1));
+    assertThat(file.isFile(), is(false));
   }
 }
