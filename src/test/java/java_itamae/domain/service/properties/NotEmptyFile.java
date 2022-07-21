0c87edde984ad632b60734363dfac0d3eb624be3
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-22 01:50:53 +0900

    modified: src/test/java/java_itamae/domain/service/properties/NotEmptyFile.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/properties/NotEmptyFile.java
+++ src/test/java/java_itamae/domain/service/properties/NotEmptyFile.java
@@ -4,10 +4,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
-import java_itamae.domain.model.contents.ContentsModel;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java_itamae.domain.model.contents.ContentsModel;
 
 /** ファイルが存在して内容が空ではない場合のテスト */
 public class NotEmptyFile {
@@ -18,10 +20,11 @@ public class NotEmptyFile {
     file = new File("test.txt");
     file.createNewFile();
 
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
     service.createProperty("property1", "1 つ目のプロパティ");
     service.createProperty("property2", "2 つ目のプロパティ");
   }
@@ -32,88 +35,94 @@ public class NotEmptyFile {
   }
 
   /**
-   * すでに存在するプロパティーキーを対象に {@link PropertiesService#createProperty(String, String)} を実行した場合に {@link
-   * Exception} が送出されること。
+   * すでに存在するプロパティーキーを対象に {@link PropertiesService#createProperty(String, String)} を実行した場合に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
    */
-  @Test(expected = Exception.class)
+  @Test
   public void pse001() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
 
-    try {
-      service.createProperty("property1", "登録テスト");
-    } catch (final Exception e) {
-      assertThat(service.getProperty("property1"), is("1 つ目のプロパティ"));
-      System.err.println(e);
-      throw e;
-    }
+    final int status = service.createProperty("property1", "登録テスト");
+    assertThat(status, is(1));
+    assertThat(service.getProperty("property1"), is("1 つ目のプロパティ"));
   }
 
   /** {@link PropertiesService#getProperty(String)} 実行時に引数に指定したキーの値を取得できること。 */
   @Test
   public void pss001() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
     assertThat(service.getProperty("property1"), is("1 つ目のプロパティ"));
     assertThat(service.getProperty("property2"), is("2 つ目のプロパティ"));
   }
 
   /**
-   *
+   * {@link PropertiesService#updateProperty(String, String)} 実行時に
    *
    * <ul>
-   *   <li>{@link PropertiesService#updateProperty(String, String)} 実行時にプロパティの値を上書きできること。
-   *   <li>終了ステータスが true であること。
+   *   <li>プロパティの値を上書きできること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void pss002() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-    final boolean status = service.updateProperty("property2", "更新テスト");
-    assertThat(status, is(true));
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+
+    final int status = service.updateProperty("property2", "更新テスト");
+    assertThat(status, is(2));
     assertThat(service.getProperty("property2"), is("更新テスト"));
     assertThat(service.getProperty("property1"), is("1 つ目のプロパティ"));
   }
 
   /**
-   * {@link PropertiesService#updateProperty(String, String)} 実行時にプロパティの値が変更前と同一である場合に終了ステータスが false
+   * {@link PropertiesService#updateProperty(String, String)} 実行時にプロパティの値が変更前と同一である場合に終了ステータスが 0
    * であること。
    */
   @Test
   public void pss003() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-    final boolean status = service.updateProperty("property1", "1 つ目のプロパティ");
-    assertThat(status, is(false));
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+    final int status = service.updateProperty("property1", "1 つ目のプロパティ");
+    assertThat(status, is(0));
     assertThat(service.getProperty("property1"), is("1 つ目のプロパティ"));
     assertThat(service.getProperty("property2"), is("2 つ目のプロパティ"));
   }
 
   /**
-   *
+   * {@link PropertiesService#deleteProperty(String)} 実行時に
    *
    * <ul>
-   *   <li>{@link PropertiesService#deleteProperty(String)} 実行時に指定したプロパティを削除できること。
-   *   <li>終了ステータスが true であること。
+   *   <li>指定したプロパティを削除できること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void pss004() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-    final boolean status = service.deleteProperty("property1");
-    assertThat(status, is(true));
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+    final int status = service.deleteProperty("property1");
+    assertThat(status, is(2));
     assertThat(service.getProperty("property2"), is("2 つ目のプロパティ"));
 
     try {
