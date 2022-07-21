a4effc0fafd206234f3cdf38cea96b7c979db7a1
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-22 01:50:03 +0900

    modified: src/test/java/java_itamae/domain/service/properties/EmptyFile.java
    テスト対象クラスの仕様変更に伴うコード修正。

--- src/test/java/java_itamae/domain/service/properties/EmptyFile.java
+++ src/test/java/java_itamae/domain/service/properties/EmptyFile.java
@@ -4,12 +4,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java_itamae.domain.common.GetTestEncoding;
-import java_itamae.domain.model.contents.ContentsModel;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java_itamae.domain.common.GetTestEncoding;
+import java_itamae.domain.model.contents.ContentsModel;
 
 /** ファイルが存在して空である場合のテスト */
 public class EmptyFile {
@@ -31,10 +32,11 @@ public class EmptyFile {
   /** {@link PropertiesService#getProperty(String)} 実行時に {@link Exception} が送出されること。 */
   @Test(expected = Exception.class)
   public void pse001() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.properties");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
 
     try {
       service.getProperty("test");
@@ -45,78 +47,83 @@ public class EmptyFile {
   }
 
   /**
-   * {@link PropertiesService#updateProperty(String, String)} 実行時に {@link FileNotFoundException}
-   * が送出されること。
-   */
-  @Test(expected = FileNotFoundException.class)
-  public void pse002() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
-
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-
-    try {
-      service.updateProperty("test", "更新テスト");
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
-  }
-
-  /**
-   * {@link PropertiesService#deleteProperty(String)} 実行時に {@link FileNotFoundException} が送出されること。
-   */
-  @Test(expected = FileNotFoundException.class)
-  public void pse003() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.txt");
-
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-
-    try {
-      service.deleteProperty("test");
-    } catch (final Exception e) {
-      System.err.println(e);
-      throw e;
-    }
-  }
-
-  /**
-   *
+   * {@link PropertiesService#updateProperty(String, String)} 実行時に
    *
    * <ul>
-   *   <li>{@link PropertiesService#createProperty(String, String)} 実行時にプロパティファイルへの書込みができること。
-   *   <li>終了ステータスが true であること。
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
+  @Test
+  public void pse002() throws Exception {
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
+
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+
+    final int status = service.updateProperty("test", "更新テスト");
+    assertThat(status, is(1));
+  }
+
+  /**
+   * {@link PropertiesService#deleteProperty(String)} 実行時に
+   *
+   * <ul>
+   *   <li>異常終了すること。
+   *   <li>終了ステータスが 1 であること。
+   * </ul>
+   */
+  @Test
+  public void pse003() throws Exception {
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
+
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+
+    final int status = service.deleteProperty("test");
+    assertThat(status, is(1));
+  }
+
+  /**
+   * {@link PropertiesService#createProperty(String, String)} 実行時に
+   *
+   * <ul>
+   *   <li>プロパティファイルへの書込みができること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void pss001() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.properties");
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-    final boolean status = service.createProperty("test", "登録テスト");
-    assertThat(status, is(true));
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+    final int status = service.createProperty("test", "登録テスト");
+    assertThat(status, is(2));
     assertThat(service.getProperty("test"), is("登録テスト"));
   }
 
   /**
-   *
+   * 文字エンコーディングを指定して {@link PropertiesService#createProperty(String, String)} を実行した場合に
    *
    * <ul>
-   *   <li>文字エンコーディングを指定した場合にプロパティファイルへの書込みができること。
-   *   <li>終了ステータスが true であること。
+   *   <li>プロパティファイルへの書込みができること。
+   *   <li>終了ステータスが 2 であること。
    * </ul>
    */
   @Test
   public void pss002() throws Exception {
-    final ContentsModel attr = new ContentsModel();
-    attr.setPath("test.properties");
-    attr.setEncoding(getTestEncoding.get());
+    final ContentsModel model = new ContentsModel();
+    model.setPath(file.getPath());
+    model.setEncoding(getTestEncoding.get());
 
-    final PropertiesService service = new PropertiesServiceImpl(attr);
-    final boolean status = service.createProperty("test", "登録テスト");
-    assertThat(status, is(true));
+    final PropertiesService service = new PropertiesServiceImpl();
+    service.init(model);
+    final int status = service.createProperty("test", "登録テスト");
+    assertThat(status, is(2));
     assertThat(service.getProperty("test"), is("登録テスト"));
   }
 }
