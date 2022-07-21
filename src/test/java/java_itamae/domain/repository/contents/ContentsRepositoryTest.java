7d8f994e74aca363025d2f41b2b60e71574d09bc
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-22 01:06:57 +0900

    renamed: src/test/java/java_itamae/domain/{repository -> component}/contents

--- src/test/java/java_itamae/domain/repository/contents/ContentsRepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package java_itamae.domain.repository.contents;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({EmptyFile.class, NotEmptyFile.class})
-public class ContentsRepositoryTest {}
