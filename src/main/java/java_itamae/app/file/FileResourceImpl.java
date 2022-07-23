88c3d8d9257efc511ec9ab8acf9ba3d38b7f3588
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-23 14:49:37 +0900

    new file: src/main/java/java_itamae/app/file/FileResourceImpl.java
    deleted: src/main/java/java_itamae/app/file/{CreateFile.java, DeleteFile.java}
    deleted: src/main/java/java_itamae/domain/model/file/FileResourceModelValidator.java
    
    FileResourceImpl へ集約。

---
+++ src/main/java/java_itamae/app/file/FileResourceImpl.java
@@ -0,0 +1,45 @@
+package java_itamae.app.file;
+
+import java.util.Map;
+
+import java_itamae.app.common.BaseResource;
+import java_itamae.domain.model.file.FileResourceModel;
+import java_itamae.domain.service.file.FileService;
+import java_itamae.domain.service.file.FileServiceImpl;
+
+public class FileResourceImpl implements BaseResource<FileResourceModel> {
+
+  @Override
+  public FileResourceModel convertToModel(Map<String, String> properties) {
+    final FileResourceModel model = new FileResourceModel();
+
+    model.setAction(properties.get("action"));
+    model.setPath(properties.get("path"));
+    model.setGroup(properties.get("group"));
+    model.setMode(properties.get("mode"));
+    model.setOwner(properties.get("owner"));
+
+    return model;
+  }
+
+  @Override
+  public int apply(Map<String, String> properties) {
+    int status = 0;
+    final FileResourceModel model = this.convertToModel(properties);
+
+    if (!this.validate(model)) {
+      status = 1;
+      return status;
+    }
+
+    final FileService service = new FileServiceImpl();
+
+    if (model.getAction().equals("create")) {
+      status = service.create(model);
+    } else if (model.getAction().equals("delete")) {
+      status = service.delete(model);
+    }
+
+    return status;
+  }
+}
