f140745134c72b66853b4f314034ac654786b6df
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-23 16:41:57 +0900

    new file: src/test/java/java_itamae/domain/common/GetTestContents.java

---
+++ src/test/java/java_itamae/domain/common/GetTestContents.java
@@ -0,0 +1,17 @@
+package java_itamae.domain.common;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class GetTestContents implements Supplier<List<String>> {
+
+  @Override
+  public List<String> get() {
+    final List<String> contents = new ArrayList<>();
+    contents.add("1 行目");
+    contents.add("2 行目");
+
+    return contents;
+  }
+}
