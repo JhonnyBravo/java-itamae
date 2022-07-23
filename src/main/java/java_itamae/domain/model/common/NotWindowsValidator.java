530f5ef14f7c98b8a5b9259aac1a6a17e37891a7
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-23 14:44:25 +0900

    renamed: src/main/java/java_itamae/domain/{model -> common}/file/NotWindowsValidator.java

--- src/main/java/java_itamae/domain/model/common/NotWindowsValidator.java
+++ src/main/java/java_itamae/domain/model/common/NotWindowsValidator.java
@@ -1,4 +1,4 @@
-package java_itamae.domain.model.file;
+package java_itamae.domain.model.common;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
