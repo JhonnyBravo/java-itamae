81943b74fa5954bec1609bfe27af8cb4f8ca80cf
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-07-23 15:51:40 +0900

    renamed: src/main/java/java_itamae/app/{template -> properties}/GetProperties.java

--- src/main/java/java_itamae/app/properties/GetProperties.java
+++ src/main/java/java_itamae/app/properties/GetProperties.java
@@ -1,13 +1,15 @@
-package java_itamae.app.template;
+package java_itamae.app.properties;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /** プロパティファイルのからキーと値を読み込む。 */
 public class GetProperties implements Function<ContentsModel, Map<String, String>> {
@@ -19,11 +21,12 @@ public class GetProperties implements Function<ContentsModel, Map<String, String
    * @return properties キーと値を収めた {@link Map}
    */
   public Map<String, String> apply(ContentsModel model) {
-    final PropertiesService service = new PropertiesServiceImpl(model);
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     Map<String, String> properties = new HashMap<>();
 
     try {
+      final PropertiesService service = new PropertiesServiceImpl();
+      service.init(model);
       properties = service.getProperties();
     } catch (final Exception e) {
       logger.warn(e.toString());
