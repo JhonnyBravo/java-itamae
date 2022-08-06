96a815c3f9697ae66d2290bfd936c14e790d553d
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-06 21:20:02 +0900

    renamed: src/main/java/java_itamae/{app/Main.java -> Main.java}
    SpringBoot 導入に伴うコード修正。
    
    パッケージスキャンの都合で Main.java の配置場所を修正。

--- src/main/java/java_itamae/Main.java
+++ src/main/java/java_itamae/Main.java
@@ -1,8 +1,10 @@
-package java_itamae.app;
+package java_itamae;
 
 import gnu.getopt.Getopt;
 import gnu.getopt.LongOpt;
 import java.util.Map;
+
+import java_itamae.app.Usage;
 import java_itamae.app.common.BaseResource;
 import java_itamae.app.directory.DirectoryResourceImpl;
 import java_itamae.app.file.FileResourceImpl;
@@ -12,8 +14,12 @@ import java_itamae.app.template.TemplateResourceImpl;
 import java_itamae.domain.model.contents.ContentsModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /** プロパティファイルを読込み、ファイルの設定値に従って動作を実行する。 */
+@SpringBootApplication(scanBasePackages = "java_itamae")
 public class Main {
   /**
    * プロパティファイルを読込み、動作を実行する。
@@ -25,6 +31,14 @@ public class Main {
    *     </ul>
    */
   public static void main(String[] args) {
+    final ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+    final Usage usage = context.getBean(Usage.class);
+    final ContentsModelValidator validator = context.getBean(ContentsModelValidator.class);
+    final GetProperties getProperties = context.getBean(GetProperties.class);
+    final FileResourceImpl fileResource = context.getBean(FileResourceImpl.class);
+    final DirectoryResourceImpl directoryResource = context.getBean(DirectoryResourceImpl.class);
+    final TemplateResourceImpl templateResource = context.getBean(TemplateResourceImpl.class);
+
     final LongOpt[] longopts = new LongOpt[2];
     longopts[0] = new LongOpt("path", LongOpt.REQUIRED_ARGUMENT, null, 'p');
     longopts[1] = new LongOpt("encoding", LongOpt.REQUIRED_ARGUMENT, null, 'e');
@@ -35,7 +49,6 @@ public class Main {
 
     int c;
     int status = 0;
-    final Usage usage = new Usage();
 
     if (args.length == 0) {
       usage.run();
@@ -57,26 +70,25 @@ public class Main {
       }
     }
 
-    if (!new ContentsModelValidator().test(cliArgs)) {
+    if (!validator.test(cliArgs)) {
       status = 1;
       System.exit(status);
     }
 
     // プロパティファイルからキーと値を読み込む。
-    final GetProperties getProperties = new GetProperties();
     final Map<String, String> properties = getProperties.apply(cliArgs);
 
     // resource_name を取得し、実行するリソースを判定する。
     final String resourceName = properties.get("resource_name");
 
     if (resourceName != null && resourceName.equals("file")) {
-      final BaseResource<?> resource = new FileResourceImpl();
+      final BaseResource<?> resource = fileResource;
       status = resource.apply(properties);
     } else if (resourceName != null && resourceName.equals("directory")) {
-      final BaseResource<?> resource = new DirectoryResourceImpl();
+      final BaseResource<?> resource = directoryResource;
       status = resource.apply(properties);
     } else if (resourceName != null && resourceName.equals("template")) {
-      final BaseResource<?> resource = new TemplateResourceImpl();
+      final BaseResource<?> resource = templateResource;
       status = resource.apply(properties);
     } else {
       final Logger logger = LoggerFactory.getLogger(Main.class);
