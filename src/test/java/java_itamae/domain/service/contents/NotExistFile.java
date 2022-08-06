5937a85a76c84080316b0b4a9fb2e04e86d05096
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-06 14:04:03 +0900

    modified: src/test/java/java_itamae/domain/service/contents/NotExistFile.java
    SpringBootTest 導入に伴うコード修正。

--- src/test/java/java_itamae/domain/service/contents/NotExistFile.java
+++ src/test/java/java_itamae/domain/service/contents/NotExistFile.java
@@ -5,26 +5,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java_itamae.domain.common.GetTestContents;
-import java_itamae.domain.model.contents.ContentsModel;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java_itamae.domain.common.GetTestContents;
+import java_itamae.domain.model.contents.ContentsModel;
 
 /** ファイルが存在しない場合のテスト */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class NotExistFile {
   private File file;
-  private GetTestContents getTestContents;
-  private ContentsService service;
+  @Autowired private GetTestContents getTestContents;
+  @Autowired private ContentsService service;
 
   @Before
   public void setUp() throws Exception {
-    getTestContents = new GetTestContents();
     file = new File("NotExist.txt");
 
     final ContentsModel model = new ContentsModel();
     model.setPath(file.getPath());
 
-    service = new ContentsServiceImpl();
     service.init(model);
   }
 
