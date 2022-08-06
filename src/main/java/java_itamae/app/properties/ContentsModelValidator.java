d56535beb1adcbd39a8334740d8b19e3c1fcfe47
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-06 21:26:04 +0900

    modified: src/main/java/java_itamae/app/properties/ContentsModelValidator.java
    SpringBoot 導入に伴うコード修正。

--- src/main/java/java_itamae/app/properties/ContentsModelValidator.java
+++ src/main/java/java_itamae/app/properties/ContentsModelValidator.java
@@ -1,15 +1,17 @@
 package java_itamae.app.properties;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
 import java.util.Set;
 import java.util.function.Predicate;
 import java_itamae.domain.model.contents.ContentsModel;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /** {@link ContentsModel} のバリデーションチェックを実行する。 */
+@Service
 public class ContentsModelValidator implements Predicate<ContentsModel> {
   /**
    * {@link ContentsModel} のバリデーションチェックを実行する。
@@ -27,18 +29,17 @@ public class ContentsModelValidator implements Predicate<ContentsModel> {
     final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
     final Set<ConstraintViolation<ContentsModel>> resultSet = validator.validate(model);
 
-    if (resultSet.size() > 0) {
-      resultSet.stream()
-          .forEach(
-              error -> {
-                final String path = error.getPropertyPath().toString();
-                final String message = error.getMessage();
-                logger.warn(path + ": " + message);
-              });
-
-      return false;
-    } else {
+    if (resultSet.size() <= 0) {
       return true;
     }
+    resultSet.stream()
+        .forEach(
+            error -> {
+              final String path = error.getPropertyPath().toString();
+              final String message = error.getMessage();
+              logger.warn(path + ": " + message);
+            });
+
+    return false;
   }
 }
