597fac69f68d23de8b21a19eacd9665c5a2edf43
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-06 13:56:05 +0900

    modified: src/test/java/java_itamae/domain/service/file/NotExistFile.java
    SpringBootTest 導入に伴うコード修正。

--- src/test/java/java_itamae/domain/service/file/NotExistFile.java
+++ src/test/java/java_itamae/domain/service/file/NotExistFile.java
@@ -6,18 +6,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java_itamae.domain.model.contents.ContentsModel;
-import java_itamae.domain.model.file.FileResourceModel;
-import java_itamae.domain.service.properties.PropertiesService;
-import java_itamae.domain.service.properties.PropertiesServiceImpl;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java_itamae.domain.model.contents.ContentsModel;
+import java_itamae.domain.model.file.FileResourceModel;
+import java_itamae.domain.service.properties.PropertiesService;
 
 /** ファイルが存在しない場合のテスト */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class NotExistFile {
-  private FileService fs;
-  private PropertiesService ps;
+  @Autowired private FileService fs;
+  @Autowired private PropertiesService ps;
   private FileResourceModel model;
   private Path path;
 
@@ -25,10 +32,8 @@ public class NotExistFile {
   public void setUp() throws Exception {
     final ContentsModel ca = new ContentsModel();
     ca.setPath("src/test/resources/test.properties");
-    ps = new PropertiesServiceImpl();
     ps.init(ca);
 
-    fs = new FileServiceImpl();
     model = new FileResourceModel();
     model.setPath("test.txt");
 
