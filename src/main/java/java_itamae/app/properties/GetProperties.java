e54013c781c895bbbb1e11c9e138175739e56855
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-06 21:28:07 +0900

    modified: src/main/java/java_itamae/app/properties/GetProperties.java
    SpringBoot 導入に伴うコード修正。

--- src/main/java/java_itamae/app/properties/GetProperties.java
+++ src/main/java/java_itamae/app/properties/GetProperties.java
@@ -5,12 +5,16 @@ import java.util.Map;
 import java.util.function.Function;
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.service.properties.PropertiesService;
-import java_itamae.domain.service.properties.PropertiesServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /** プロパティファイルのからキーと値を読み込む。 */
+@Service
 public class GetProperties implements Function<ContentsModel, Map<String, String>> {
+  @Autowired private PropertiesService service;
+
   @Override
   /**
    * プロパティファイルからキーと値を読込み、 {@link Map} 変換して返す。
@@ -23,7 +27,6 @@ public class GetProperties implements Function<ContentsModel, Map<String, String
     Map<String, String> properties = new HashMap<>();
 
     try {
-      final PropertiesService service = new PropertiesServiceImpl();
       service.init(model);
       properties = service.getProperties();
     } catch (final Exception e) {
