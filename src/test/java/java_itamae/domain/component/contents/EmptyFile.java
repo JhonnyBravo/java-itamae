74e7db2114204971d7dbb38d5c07d182345cd160
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-06 13:21:25 +0900

    modified: src/test/java/java_itamae/domain/component/contents/EmptyFile.java
    SpringBootTest 導入に伴うコード修正。

--- src/test/java/java_itamae/domain/component/contents/EmptyFile.java
+++ src/test/java/java_itamae/domain/component/contents/EmptyFile.java
@@ -6,26 +6,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java_itamae.domain.common.GetTestContents;
-import java_itamae.domain.common.GetTestEncoding;
-import java_itamae.domain.model.contents.ContentsModel;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java_itamae.domain.common.GetTestContents;
+import java_itamae.domain.common.GetTestEncoding;
+import java_itamae.domain.model.contents.ContentsModel;
 
 /** ファイルが空である場合のテスト。 */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EmptyFile {
-  private ContentsComponent component;
-  private GetTestEncoding getTestEncoding;
-  private GetTestContents getTestContents;
+  @Autowired private ContentsComponent component;
+  @Autowired private GetTestEncoding getTestEncoding;
+  @Autowired private GetTestContents getTestContents;
   private Path path;
 
   @Before
   public void setUp() throws Exception {
-    getTestEncoding = new GetTestEncoding();
-    getTestContents = new GetTestContents();
-
-    component = new ContentsComponentImpl();
     path = component.convertToPath("test.txt");
     Files.createFile(path);
   }
