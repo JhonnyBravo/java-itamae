51f3289ec2ad2ad691bda9406b1369950d239452
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-25 02:25:44 +0900

    modified: src/test/java/java_itamae/domain/service/contents/NotEmptyFile.java
    javax ネームスペースを jakarta ネームスペースへ修正。

--- src/test/java/java_itamae/domain/service/contents/NotEmptyFile.java
+++ src/test/java/java_itamae/domain/service/contents/NotEmptyFile.java
@@ -3,13 +3,13 @@ package java_itamae.domain.service.contents;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import jakarta.inject.Inject;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java_itamae.domain.common.GetTestContents;
 import java_itamae.domain.component.contents.ContentsComponentImpl;
 import java_itamae.domain.model.contents.ContentsModel;
-import javax.inject.Inject;
 import org.jboss.weld.junit4.WeldInitiator;
 import org.junit.After;
 import org.junit.Before;
