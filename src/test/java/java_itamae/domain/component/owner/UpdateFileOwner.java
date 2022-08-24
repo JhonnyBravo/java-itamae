29314238c0f521386329403c1791404eda3f1cd3
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-25 02:02:44 +0900

    modified: src/test/java/java_itamae/domain/component/owner/UpdateFileOwner.java
    javax ネームスペースを jakarta ネームスペースへ修正。

--- src/test/java/java_itamae/domain/component/owner/UpdateFileOwner.java
+++ src/test/java/java_itamae/domain/component/owner/UpdateFileOwner.java
@@ -3,6 +3,7 @@ package java_itamae.domain.component.owner;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import jakarta.inject.Inject;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.UserPrincipal;
@@ -10,7 +11,6 @@ import java_itamae.domain.component.properties.PropertiesComponentImpl;
 import java_itamae.domain.model.contents.ContentsModel;
 import java_itamae.domain.service.properties.PropertiesService;
 import java_itamae.domain.service.properties.PropertiesServiceImpl;
-import javax.inject.Inject;
 import org.jboss.weld.junit4.WeldInitiator;
 import org.junit.After;
 import org.junit.Before;
