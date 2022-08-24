6560b21ef0e75d2f39058e3087163cc46010cca0
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-08-25 01:43:54 +0900

    modified: src/main/java/java_itamae/domain/model/common/NotWindowsValidator.java
    javax ネームスペースを jakarta ネームスペースへ修正。

--- src/main/java/java_itamae/domain/model/common/NotWindowsValidator.java
+++ src/main/java/java_itamae/domain/model/common/NotWindowsValidator.java
@@ -1,7 +1,7 @@
 package java_itamae.domain.model.common;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 /** OS が Windows であるかどうかを確認する。 */
 public class NotWindowsValidator implements ConstraintValidator<NotWindows, String> {
