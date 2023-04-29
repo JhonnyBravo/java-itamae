f5bafa8fb1da96fd4cf4a8560b9f61e76cae9018
Jhonny Bravo <sanfranceshika5@gmail.com>
2023-04-29 20:05:24 +0900

    modified: src/main/java/java_itamae/domain/service/directory/DirectoryService.java
    modified functions: create, delete
      返り値の型を Status に修正。
      例外発生時に Exception を投げるように修正。

--- src/main/java/java_itamae/domain/service/directory/DirectoryService.java
+++ src/main/java/java_itamae/domain/service/directory/DirectoryService.java
@@ -1,6 +1,11 @@
 package java_itamae.domain.service.directory;
 
+import java_itamae.domain.component.directory.DirectoryComponent;
+import java_itamae.domain.component.group.GroupComponent;
+import java_itamae.domain.component.mode.ModeComponent;
+import java_itamae.domain.component.owner.OwnerComponent;
 import java_itamae.domain.model.directory.DirectoryResourceModel;
+import java_itamae.domain.model.status.Status;
 import java_itamae.domain.service.common.BaseService;
 
 /** ディレクトリの操作を管理する。 */
@@ -8,26 +13,48 @@ public interface DirectoryService extends BaseService {
   /**
    * ディレクトリを作成し、所有者・グループ所有者・パーミッションの設定を変更する。
    *
+   * <ol>
+   *   <li>変数 status へ {@link Status#INIT} を設定する。
+   *   <li>{@link DirectoryComponent#create(String, boolean)} を実行し、返り値を変数 status へ設定する。
+   *   <li>{@link DirectoryResourceModel#getOwner()} を実行し、値が null であるか確認する。
+   *       <ul>
+   *         <li>null である場合、後続処理へ遷移する。
+   *         <li>null ではない場合、 {@link OwnerComponent#updateOwner(String, String)} を実行し、返り値を変数 status
+   *             へ設定する。
+   *       </ul>
+   *   <li>{@link DirectoryResourceModel#getGroup()} を実行し、値が null であるか確認する。
+   *       <ul>
+   *         <li>null である場合、後続処理へ遷移する。
+   *         <li>null ではない場合、 {@link GroupComponent#updateGroup(String, String)} を実行し、返り値を変数 status
+   *             へ設定する。
+   *       </ul>
+   *   <li>{@link DirectoryResourceModel#getMode()} を実行し、値が null であるか確認する。
+   *       <ul>
+   *         <li>null である場合、後続処理へ遷移する。
+   *         <li>null ではない場合、 {@link ModeComponent#updateMode(String, String)} を実行し、返り値を変数 status
+   *             へ設定する。
+   *       </ul>
+   *   <li>返り値として変数 status を返す。
+   * </ol>
+   *
    * @param model 操作対象とするディレクトリの情報を納めた {@link DirectoryResourceModel} を指定する。
-   * @return status
-   *     <ul>
-   *       <li>0: 操作を実行せずに正常終了したことを表す。
-   *       <li>1: 異常終了したことを表す。
-   *       <li>2: 操作を実行して正常終了したことを表す。
-   *     </ul>
+   * @return status {@link Status} を返す。
+   * @throws Exception 操作実行中に発生した例外を投げる。
    */
-  int create(DirectoryResourceModel model);
+  Status create(DirectoryResourceModel model) throws Exception;
 
   /**
    * ディレクトリを削除する。
    *
+   * <ol>
+   *   <li>変数 status へ {@link Status#INIT} を設定する。
+   *   <li>{@link DirectoryComponent#delete(String, boolean)} を実行し、返り値を変数 status へ設定する。
+   *   <li>返り値として変数 status を返す。
+   * </ol>
+   *
    * @param model 操作対象とするディレクトリの情報を納めた {@link DirectoryResourceModel} を指定する。
-   * @return status
-   *     <ul>
-   *       <li>0: 操作を実行せずに正常終了したことを表す。
-   *       <li>1: 異常終了したことを表す。
-   *       <li>2: 操作を実行して正常終了したことを表す。
-   *     </ul>
+   * @return status {@link Status} を返す。
+   * @throws Exception ディレクトリ削除中に発生した例外を投げる。
    */
-  int delete(DirectoryResourceModel model);
+  Status delete(DirectoryResourceModel model) throws Exception;
 }
