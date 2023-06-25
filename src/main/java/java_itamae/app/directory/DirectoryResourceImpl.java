7907c4971cb0afdb18c795fa5a3446cc6d39b247
Jhonny Bravo <sanfranceshika5@gmail.com>
2023-06-25 23:32:19 +0900

    modified: src/main/java/java_itamae/app/directory/DirectoryResourceImpl.java
    modified method: apply
      ログメッセージの出力内容を修正。

--- src/main/java/java_itamae/app/directory/DirectoryResourceImpl.java
+++ src/main/java/java_itamae/app/directory/DirectoryResourceImpl.java
@@ -6,6 +6,7 @@ import java_itamae.domain.model.directory.DirectoryResourceModel;
 import java_itamae.domain.model.status.Status;
 import java_itamae.domain.service.directory.DirectoryService;
 import java_itamae.domain.service.directory.DirectoryServiceImpl;
+import org.slf4j.Logger;
 
 /** ディレクトリの操作を管理する。 */
 public class DirectoryResourceImpl implements BaseResource<DirectoryResourceModel> {
@@ -35,6 +36,10 @@ public class DirectoryResourceImpl implements BaseResource<DirectoryResourceMode
     final DirectoryResourceModel model = this.convertToModel(properties);
 
     if (this.validate(model)) {
+      final Logger logger = this.getLogger();
+      final String infoMsg = "resource_name: {}, action: {}, path: {}";
+      logger.info(infoMsg, model.getResourceName(), model.getAction(), model.getPath());
+
       final DirectoryService service = new DirectoryServiceImpl();
 
       try {
@@ -45,7 +50,8 @@ public class DirectoryResourceImpl implements BaseResource<DirectoryResourceMode
         }
       } catch (Exception e) {
         status = Status.ERROR;
-        this.getLogger().warn(e.toString());
+        final String warnMsg = e.toString();
+        logger.warn("{}", warnMsg);
       }
     } else {
       status = Status.ERROR;
